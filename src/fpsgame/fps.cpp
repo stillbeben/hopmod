4be410ec3877877d9fee5011fee63a8f390dc5ec
gdaws <gdaws>
2008-09-28 22:55:10 +0000

    Temporary
    git-svn-id: http://hopmod.googlecode.com/svn/trunk@280 d7818858-e1b4-11dd-a738-5fe2e3cfbcc5

--- src/fpsgame/fps.cpp
+++ src/fpsgame/fps.cpp
@@ -3,7 +3,7 @@
 #include "iengine.h"
 #include "igame.h"
 #include "game.h"
-#include "fpsserver.h"
+#include "fpsserver.hpp"
 #include "crypto.h"
 
 REGISTERGAME(fpsgame, "fps", NULL, new fpsserver());
