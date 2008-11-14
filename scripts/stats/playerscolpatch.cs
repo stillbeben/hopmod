c073f08cc37fbbbb82b9d4efe01a91bf4bef99e3
gdaws <gdaws>
2008-11-14 21:49:20 +0000

    Added players column to match table. Added stats db patch script.
    git-svn-id: http://hopmod.googlecode.com/svn/trunk@377 d7818858-e1b4-11dd-a738-5fe2e3cfbcc5

---
+++ scripts/stats/playerscolpatch.cs
@@ -0,0 +1,12 @@
+
+try statsdb eval [select players from matches where 0 = 1] [] [
+    log_status "Applying stats db update patch..."
+    statsdb eval [alter table matches add players integer;] []
+    local max 0
+    statsdb eval [select max(id) from matches] [max = (column "max(id)")]
+    log_status [Need to update @max records] 
+    loop i $max [
+        local match_id $i
+        statsdb eval [update matches set players = (select count(*) from players where match_id = $match_id) where id = $match_id] []
+    ]
+]
