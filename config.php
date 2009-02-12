0ac1c053962d4882e1bf65edf28195a12ed57395
obi12341 <obi12341>
2009-02-12 14:23:53 +0000

    fix something
    git-svn-id: http://hopmod.googlecode.com/svn/branches/web@582 d7818858-e1b4-11dd-a738-5fe2e3cfbcc5

--- config.php
+++
@@ -1,6 +0,0 @@
-<?php
-$sname = "your server name"; // Server Name
-$path_to_log = "/your/path/to/hopmod/logs/server.log";
-$admin_pw_control = 1; // PW = Server Password
-$mini_httpd_port = 7894; // Look in the sauer-server config, you dont need to change it normally
-?>
