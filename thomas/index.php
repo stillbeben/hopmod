c64146e51c220a620b5f57cd3d32d364fd13c370
obi12341 <obi12341>
2009-02-12 14:22:00 +0000

    fix web branche
    git-svn-id: http://hopmod.googlecode.com/svn/branches/web@581 d7818858-e1b4-11dd-a738-5fe2e3cfbcc5

---
+++ thomas/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN"
+   "http://www.w3.org/TR/html4/frameset.dtd">
+<html>
+<head>
+<title>Server Panel</title>
+</head>
+<frameset cols="150,*">
+  <frame src="links.html" name="Navigation">
+  <frame src="players.php" name="Daten">
+  <noframes>
+    <body bgcolor="grey">
+      <p>...update your browser...</p>
+    </body>
+  </noframes>
+</frameset>
+</html>
+
