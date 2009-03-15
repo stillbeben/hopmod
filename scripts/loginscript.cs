02d85dcba92db534e8e53c65698f84c5913dade3
obi12341 <obi12341>
2009-03-15 11:19:46 +0000

    added experimental loginscript.
    git-svn-id: http://hopmod.googlecode.com/svn/branches/ctf_256@644 d7818858-e1b4-11dd-a738-5fe2e3cfbcc5

---
+++ scripts/loginscript.cs
@@ -0,0 +1,144 @@
+playercmd_login = [
+        local loginpassword ""
+        local loginip ""
+        local firstlogin ""
+        local ipaddr ""
+	local greet ""
+	local adminlvl ""
+
+        if (= (player_var $cn logged_in) 1) [privmsg $cn (red[You are already logged in])][
+
+         statsdb eval [select password from register where name=$arg1] [loginpassword = (column password)]
+         statsdb eval [select firstlogin from register where name=$arg1] [firstlogin = (column firstlogin)]
+         statsdb eval [select ipaddr from register where name=$arg1] [loginip = (column ipaddr)]
+	 statsdb eval [select greet from register where name=$arg1] [greet = (column greet)]
+	 statsdb eval [select admin from register where name =$arg1] [adminlvl = (column admin)]
+	
+	 if (strcmp $arg2 "") [privmsg $cn (format "%1" (red [Nice try!]) )] [
+         if (strcmp $loginpassword $arg2) [
+                if (= $firstlogin 1) [
+                        playerip = (player_ip $cn)
+                        privmsg $cn (format "%1" (blue [This is your first login!]) )
+                        statsdb eval [update register set firstlogin = 0 where name = $arg1]
+                        statsdb eval [update register set ipaddr = $playerip where name = $arg1]
+                ]
+		if (= $adminlvl 1) [
+		player_var $cn adminlvl 1]
+                player_var $cn logged_in 1
+		player_var $cn logged_in_as $arg1
+                msg (format "%1 %2 %3" (blue (player_name $cn)) (orange [is now logged in as]) (red (player_var $cn logged_in_as)) )
+                privmsg $cn (format "Last login from IP: %1" (green $loginip) )
+                privmsg $cn (format "Your IP now: %1" (green (player_ip $cn)) )
+		if (= (player_var $cn adminlvl) 1) [
+			privmsg $cn (format "Type: %1 to get master/admin." (red [#getmaster]) )
+		]
+                playerip = (player_ip $cn)
+                statsdb eval [update register set ipaddr = $playerip where name = $arg1]
+                player_var $cn mute 0
+                if (= (player_var $cn speccr) 1) [unspec $cn]
+		if (!(strcmp $greet "0")) [msg (format "Greet Message: %1 - %2" (green $arg1) (green $greet) ) ]
+        ] [ privmsg $cn (format "%1" (red [Wrong username or password!]) ) ]
+]   ]
+]
+
+playercmd_getmaster = [
+	if (&& (= (player_var $cn adminlvl) 1) (= (player_var $cn logged_in) 1)) [
+		setpriv $cn admin
+		privmsg $cn (format "Type: %1 to leave master/admin." (red [#leavemaster]) )
+	] [privmsg $cn (red [Permission Denied]) ]
+]
+
+playercmd_leavemaster = [
+        if (= (player_var $cn logged_in) 1) [
+                setpriv $cn none
+		privmsg $cn (format "Type: %1 to get master/admin." (red [#getmaster]) )
+        ] [privmsg $cn (red [Permission Denied]) ]
+]
+
+playercmd_logout = [
+        if (= (player_pvar $cn logged_in) 1) [
+        setmaster $cn 0
+	setpriv $cn none
+        player_var $cn logged_in 0
+	player_var $cn adminlvl 0
+	player_var $cn logged_in_as 0
+        msg (format "%1 %2" (blue (player_name $cn)) (orange [has been logged out]) )
+        ] [privmsg $cn (format "%1" (red [You are not logged in!]) )]
+]
+
+playercmd_register = [
+        local registername ""
+        if (= (listlen $arguments) 3) [
+        statsdb eval [select name from register where name =$arg1] [registername = (column name)]
+        if (strcmp $registername $arg1) [
+                privmsg $cn (format "%1" (red [This username is already in use!]) )
+        ] [
+                statsdb eval [INSERT INTO register VALUES(1, $arg1 , $arg2 , 0 , 0 , 0 , 'player' , 0);]
+                privmsg $cn (format "%1" (green [Your account is created, you can now login by typing #login name password]) )
+                ]
+        ] [privmsg $cn (format "%1" (red [You must enter: #register username password]) )]
+
+]
+
+playercmd_greet = [
+        if (= (player_var $cn logged_in) 1) [
+                logged_in_as_name = (player_var $cn logged_in_as)
+		msg $logged_in_as_name
+                statsdb eval [update register set greet = $arg1 where name = $logged_in_as_name]
+                privmsg $cn [test]
+	] [privmsg $cn (format "%1" (red [You are not logged in!]) )]
+]
+
+playercmd_whoisonline = [
+	if (= (player_var $cn logged_in) 1) [
+		registcn = $cn
+		privmsg $cn (format "%1     %2     %3" (green[NAME]) (blue[LOGINNAME]) (red[IP]) )
+		foreach (players) [
+		testcn = $arg1
+		if (= (player_var $arg1 logged_in) 1) [
+		msg (format "%1     %2     %3" (green (player_name $arg1)) (blue (player_var $arg1 logged_in_as)) (red (player_ip $arg1)) )
+			]
+		]  
+	] [privmsg $cn (format "%1" (red [You are not logged in!]) )]
+]
+
+playercmd_changepw = [
+	if (= (player_var $cn logged_in) 1) [
+		 logged_in_as_name = (player_var $cn logged_in_as)
+		 statsdb eval [update register set password = $arg1 where name = $logged_in_as_name ]
+		 privmsg $cn (format "Your password successful changed to: %1" (red $arg1) )
+	]
+]
+
+playercmd_changeuserpw = [
+	local changeuserpwname ""
+	if (&& (= (player_var $cn adminlvl) 1) (= (player_var $cn logged_in) 1)) [
+	
+	statsdb eval [select name from register where name =$arg1] [changeuserpwname = (column name)]
+        if (strcmp $changeuserpwname $arg1) [
+		statsdb eval [update register set password = $arg2 where name = $arg1]
+		privmsg $cn (format "Command successful! Password from %1 changed to: %2 !" $arg1 $arg2)
+	][privmsg $cn (format "%1 %2" (red[No user with name:]) (red $arg1) )]
+	]
+]
+
+
+playercmd_giveadmin = [
+	if (&& (= (player_var $cn adminlvl) 1) (= (player_var $cn logged_in) 1)) [
+		statsdb eval [update register set admin ='1' where name = $arg1]
+		privmsg $cn (format "Command successful! %1 has admin now!" $arg1)
+	]
+]
+
+playercmd_deladmin = [
+		local deladminname ""
+	        if (&& (= (player_var $cn adminlvl) 1) (= (player_var $cn logged_in) 1)) [
+	
+	        statsdb eval [select name from register where name =$arg1] [deladminname = (column name)]
+	        if (strcmp $deladminname $arg1) [
+                statsdb eval [update register set admin ='0' where name = $arg1]
+                privmsg $cn (format "Command successful! %1 has no access privilege!" $arg1)
+        	][privmsg $cn (format "%1 %2" (red[No user with name:]) (red $arg1) )]
+	]	
+]
+
