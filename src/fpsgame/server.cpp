11a8d807e45a68d2af659fb3c26cb35362cb7730
graham.daws <graham.daws>
2009-05-04 13:37:56 +0000

    Fixed bug relating to updatemaster: don't send SV_CURRENTMASTER packet to bot clients.
    git-svn-id: http://hopmod.googlecode.com/svn/branches/ng@810 d7818858-e1b4-11dd-a738-5fe2e3cfbcc5

--- src/fpsgame/server.cpp
+++ src/fpsgame/server.cpp
@@ -1762,6 +1762,7 @@ namespace server
             clientinfo *m = currentmaster>=0 ? getinfo(currentmaster) : NULL;
             loopv(clients)
             {
+                if(clients[i]->state.aitype != AI_NONE) continue;
                 if(clients[i]->privilege) sendf(clients[i]->clientnum, 1, "ri3", SV_CURRENTMASTER, clients[i]->clientnum, clients[i]->privilege);
                 else sendf(clients[i]->clientnum, 1, "ri3", SV_CURRENTMASTER, currentmaster, m ? m->privilege : 0);
             }
