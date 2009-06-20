0f1217f44d2b9e256f948e9780aa1d862a5e2041
cchristopher01 <cchristopher01>
2009-06-20 19:59:55 +0000

    added versus script #versus and sudden death script #sd #nosd
    git-svn-id: http://hopmod.googlecode.com/svn/branches/ng@924 d7818858-e1b4-11dd-a738-5fe2e3cfbcc5

--- script/playercmd.lua
+++ script/playercmd.lua
@@ -278,4 +278,181 @@ function server.playercmd_kick(cn,kick_tcn,kick_reason)
     end,cn)
 end 
 
+function server.playercmd_sd(cn, arg1)
+        return admincmd(function () 
+		sudden_death_enabled = "true" 
+		server.msg(red("--[ Sudden Death Mode Enabled. There will be no ties"))
+	end, cn)
+end
+function server.playercmd_nosd(cn, arg1)
+        return admincmd(function () 
+		sudden_death_enabled = "false" 
+		sudden_death = "false"
+		server.msg(red("--[ Sudden Death Mode Disabled. There will be ties"))
+	end, cn)
+end
+
+server.event_handler("timeupdate", function (mins)
+        if sudden_death_enabled == "true" then
+                player_score = {}
+                if endgame == "true" then endgame = ""; return 0 end
+                if mins == "1" then
+                        for index, cn in ipairs(server.players()) do
+                                player_score[index] = server.player_frags(cn)
+                        end
+                        server.sleep(59000,function()
+                                if player_score[1] == player_score[2] then
+                                        server.msg(red("--[ Sudden Death. Next Frag Wins!"))
+                                        sudden_death = "true"
+                                end
+                        end)
+                end
+
+        end     if sudden_death == "true" then return 1 else return mins end
+end)
+
+server.event_handler("frag", function (target,actor)
+        if sudden_death_enabled == "true" then
+                if sudden_death == "true" then
+                        endgame = "true"
+                        sudden_death = "false"
+                        server.changetime(0)
+                end
+        end
+end)
+
+-- 1on1 script 
+-- Version: 0.1
+-- (c) by Thomas
+-- #1on1 cn1 cn2 mode map
+ 
+var1on1 = false
+var1on1player1 = "" -- cn
+var1on1player1map = false
+var1on1player2 = "" -- cn
+var1on1player2map = false
+var1on1gamecount = 0
+ 
+ 
+server.event_handler("active", function (cn) 
+        if var1on1 then
+                if cn == var1on1player1 then 
+                        var1on1player1map = true
+                end
+                if cn == var1on1player2 then 
+                        var1on1player2map = true
+                end
+                if var1on1player1map and var1on1player2map then
+                        server.msg(orange("--[ Game started! Happy Fragging."))
+                        server.pausegame(false)
+                        var1on1player1map = false
+                        var1on1player2map = false
+                end
+        end 
+end)
+ 
+server.event_handler("mapchange", function (map, mode) 
+        if var1on1 then
+                var1on1gamecount = var1on1gamecount + 1
+                if var1on1gamecount > 1 then
+                        var1on1gamecount = 0
+                        var1on1 = false
+                        return
+                end
+                server.pausegame(true)
+                server.recorddemo()
+                server.msg(orange("--[ Starting Demo Recording"))
+                server.msg(orange("--[ Waiting until all Players loaded the Map."))
+        end 
+end)
+ 
+server.event_handler("intermission", function () 
+        if var1on1 then
+                if server.player_frags(var1on1player1) > server.player_frags(var1on1player2) then
+                        server.msg(green("--[ 1on1 Game ended - " .. blue(server.player_name(var1on1player1)) .. " won the Game!"))       
+                end
+                if server.player_frags(var1on1player1) < server.player_frags(var1on1player2) then
+                        server.msg(green("--[ 1on1 Game ended - " .. blue(server.player_name(var1on1player2)) .. " won the Game!"))
+                end
+                if server.player_frags(var1on1player1) == server.player_frags(var1on1player2) then
+                        server.msg("--[ 1on1 Game ended - No Winner!")
+                end
+                var1on1 = false
+        end
+end)
+
+server.event_handler("connect", function (cn)
+        if server.player_name(cn) .. server.player_ip(cn) == var1on1player1_id then
+		server.unspec(cn)
+		server.sleep(0,    function() server.msg(orange("--[ Opponent " .. blue(server.player_name(cn)) .. " Reconnected Resuming Game in 5")) end)
+                server.sleep(1000, function() server.msg(orange("--[ Opponent " .. blue(server.player_name(cn)) .. " Reconnected Resuming Game in 4")) end)
+                server.sleep(2000, function() server.msg(orange("--[ Opponent " .. blue(server.player_name(cn)) .. " Reconnected Resuming Game in 3")) end)
+                server.sleep(3000, function() server.msg(orange("--[ Opponent " .. blue(server.player_name(cn)) .. " Reconnected Resuming Game in 2")) end)
+                server.sleep(4000, function() server.msg(orange("--[ Opponent " .. blue(server.player_name(cn)) .. " Reconnected Resuming Game in 1")) end)
+                server.sleep(5000, function() server.pausegame(false) end)
+        end
+        if server.player_name(cn) .. server.player_ip(cn) == var1on1player2_id then
+		server.unspec(cn)
+		server.sleep(0,    function() server.msg(orange("--[ Opponent " .. blue(server.player_name(cn)) .. " Reconnected Resuming Game in 5")) end)
+                server.sleep(1000, function() server.msg(orange("--[ Opponent " .. blue(server.player_name(cn)) .. " Reconnected Resuming Game in 4")) end)
+                server.sleep(2000, function() server.msg(orange("--[ Opponent " .. blue(server.player_name(cn)) .. " Reconnected Resuming Game in 3")) end)
+                server.sleep(3000, function() server.msg(orange("--[ Opponent " .. blue(server.player_name(cn)) .. " Reconnected Resuming Game in 2")) end)
+                server.sleep(4000, function() server.msg(orange("--[ Opponent " .. blue(server.player_name(cn)) .. " Reconnected Resuming Game in 1")) end)
+                server.sleep(5000, function() server.pausegame(false) end)
+        end
+end)
+
+server.event_handler("disconnect", function (cn)
+	if server.player_name(cn) .. server.player_ip(cn) == var1on1player1_id then 
+		server.msg(red("--[ Opponent " .. blue(server.player_name(cn)) .. " Disconnected Pausing Game"))
+		server.pausegame(true)
+	end 
+	if server.player_name(cn) .. server.player_ip(cn) == var1on1player2_id then 
+		server.msg(red("--[ Opponent " .. blue(server.player_name(cn)) .. " Disconnected Pausing Game")) 
+		server.pausegame(true)
+	end
+end)
+ 
+function isplayer(arg) 
+    for i, cn in ipairs(server.players()) do 
+                if tonumber(cn) == tonumber(arg) then return true end
+    end
+    for i, cn in ipairs(server.spectators()) do
+                if tonumber(cn) == tonumber(arg) then return true end
+    end
+        return false
+end
+ 
+function isnum(arg)
+        if arg < "0" or arg > "9" then return false end
+        return true
+end
+ 
+function server.playercmd_versus(cn, player1, player2, mode, map) 
+        if tonumber(server.player_priv_code(cn)) == 0 then return end
+        if var1on1 then return end
+        if not isnum(player1) or not isnum(player2) then return end
+        if not isplayer(player1) or not isplayer(player2) then return end
+        if player1 == player2 then return end
+        var1on1 = true
+        var1on1gamecount = 0
+        var1on1player1 = player1
+        var1on1player2 = player2
+	var1on1player1_id = server.player_name(player1) .. server.player_ip(player1)
+	var1on1player2_id = server.player_name(player2) .. server.player_ip(player2)
+        server.msg(green("--[ 1on1 - " .. server.player_name(player1) .. " against " .. server.player_name(player1) .. " mode: " .. mode  .. " map: " .. map ))
+        server.specall()
+        server.unspec(player1)
+        server.unspec(player2)
+        server.mastermode = 2
+        server.pausegame(true)
+        server.sleep(0, function() server.msg(orange(	"--[ Loading the Map in 5 Seconds")) end)
+        server.sleep(1000, function() server.msg(orange("--[ Loading the Map in 4 Seconds")) end)
+        server.sleep(2000, function() server.msg(orange("--[ Loading the Map in 3 Seconds")) end)
+        server.sleep(3000, function() server.msg(orange("--[ Loading the Map in 2 Seconds")) end)
+        server.sleep(4000, function() server.msg(orange("--[ Loading the Map in 1 Second ")) end)
+        server.sleep(5000, function() server.changemap(map, mode, -1) end)
+end
+-- 1on1 script end
+
 -- ]]
