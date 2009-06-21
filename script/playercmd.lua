bd8309a15ec7b0193b4b4dacabb873802c50b734
cchristopher01 <cchristopher01>
2009-06-21 21:18:23 +0000

    fixes for sudden death mode.
    git-svn-id: http://hopmod.googlecode.com/svn/branches/ng@932 d7818858-e1b4-11dd-a738-5fe2e3cfbcc5

--- script/playercmd.lua
+++ script/playercmd.lua
@@ -280,12 +280,15 @@ end
 
 function server.playercmd_sd(cn, arg1)
         return admincmd(function () 
+		player_score = {}
 		sudden_death_enabled = "true" 
+		sudden_death = "false"
 		server.msg(red("--[ Sudden Death Mode Enabled. There will be no ties"))
 	end, cn)
 end
 function server.playercmd_nosd(cn, arg1)
         return admincmd(function () 
+		player_score = {}
 		sudden_death_enabled = "false" 
 		sudden_death = "false"
 		server.msg(red("--[ Sudden Death Mode Disabled. There will be ties"))
@@ -294,31 +297,35 @@ end
 
 server.event_handler("timeupdate", function (mins)
         if sudden_death_enabled == "true" then
-                player_score = {}
-                if endgame == "true" then endgame = "false"; sudden_death = "false" ; return 0 end
                 if mins == "1" then
-                        for index, cn in ipairs(server.players()) do
-                                player_score[index] = server.player_frags(cn)
-                        end
-                        server.sleep(59000,function()
-                                if player_score[1] == player_score[2] then
-                                        server.msg(red("--[ Sudden Death. Next Frag Wins!"))
-                                        sudden_death = "true"
-                                end
-                        end)
+			server.sleep(59000,    function() 
+	                        for index, cn in ipairs(server.players()) do
+	                                player_score[index] = server.player_frags(cn)
+	                        end
+	                        if player_score[1] == player_score[1] then
+	                        	server.msg(red("--[ Sudden Death. Next Frag Wins!"))
+		                        sudden_death = "true"
+	                        end
+				return 1
+			end)
                 end
-
-        end     if sudden_death == "true" then return 1 else return mins end
+		if sudden_death == "true" then return 1 else return mins end
+        else 
+		return mins
+	end 
 end)
 
 server.event_handler("frag", function (target,actor)
         if sudden_death_enabled == "true" then
                 if sudden_death == "true" then
-                        endgame = "true"
-                        sudden_death = "false"
+			sudden_death = "false"
                         server.changetime(0)
                 end
         end
+end)
+
+server.event_handler("mapchange", function (map, mode)
+	sudden_death = "false"
 end)
 
 -- 1on1 script 
