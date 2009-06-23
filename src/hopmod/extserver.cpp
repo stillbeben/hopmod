7c7491574272afd29d5e5d0e7067dcf5f8825d24
graham.daws <graham.daws>
2009-06-23 20:37:31 +0000

    Moved source out of floodprotection.cpp into extserver.cpp.
    git-svn-id: http://hopmod.googlecode.com/svn/branches/ng@939 d7818858-e1b4-11dd-a738-5fe2e3cfbcc5

--- src/hopmod/extserver.cpp
+++ src/hopmod/extserver.cpp
@@ -1,5 +1,11 @@
 #ifdef INCLUDE_EXTSERVER_CPP
 
+int sv_text_hit_length = 0;
+int sv_sayteam_hit_length = 0;
+int sv_mapvote_hit_length = 0;
+int sv_switchname_hit_length = 0;
+int sv_switchteam_hit_length = 0;
+
 static boost::signals::connection close_listenserver_slot;
 static bool reload = false;
 
