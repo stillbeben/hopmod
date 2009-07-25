0a94d40c07ee3f609e3d4cd1a2ed94ac075d19a0
graham.daws <graham.daws>
2009-07-25 01:22:41 +0000

    Added #eval command.
    git-svn-id: http://hopmod.googlecode.com/svn/branches/ng@1016 d7818858-e1b4-11dd-a738-5fe2e3cfbcc5

--- script/resetvars.cs
+++ script/resetvars.cs
@@ -99,6 +99,7 @@ global ping_limit_warnings 4
 
 global enable_cheater_command 0
 global enable_votekick_command 0
+global enable_eval_command 0
 
 global enable_dynamic_maxclients 0
 
--- script/servermod.cs
+++ script/servermod.cs
@@ -1,3 +1,4 @@
+do = [arg1]
 
 exec "script/playercmd.cs"
 exec "script/irc.csl"
@@ -7,4 +8,10 @@ global execCubeScriptFile &exec-cubescript
 event_handler "started" (func [] [
     if $enable_ping_limiter [exec script/pinglimiter.cs]
     if $enable_cheater_command [exec script/cheatercmd.cs]
+    
+    if $enable_eval_command [
+        global playercmd_eval (func [cn code] [
+            if (strcmp (player_priv $cn) "admin") [do [@code]] [player_msg $cn (err "Permission Denied")]
+        ])
+    ]
 ])
