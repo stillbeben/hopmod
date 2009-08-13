1b8e690ca1f5501eebfc86c04819bd5965b4abdd
graham.daws <graham.daws>
2009-08-13 22:04:06 +0000

    Removed support for script/command/*.cs commands.
    git-svn-id: http://hopmod.googlecode.com/svn/branches/ng@1043 d7818858-e1b4-11dd-a738-5fe2e3cfbcc5

--- src/hopmod/banlist.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef HOPMOD_BANLIST_HPP
-#define HOPMOD_BANLIST_HPP
-
-class banned_networks;
-
-bool write_banlist(const banned_networks *, const char *);
-bool load_banlist(const char *, banned_networks *);
-
-#endif
--- src/hopmod/process_player_command.cpp
+++ src/hopmod/process_player_command.cpp
@@ -10,50 +10,35 @@ bool eval_player_command(int cn,const char * cmdline, const std::vector<std::str
 {
     if(arguments.empty()) return false;
     
-    defformatstring(cmdfile)("./script/command/%s" CUBESCRIPT_EXTENSION ,arguments[0].c_str());
-    if(fileexists(cmdfile,"r"))
+    defformatstring(cmdname)("playercmd_%s",arguments[0].c_str());
+    fungu::script::env::object * cmdobj = env.lookup_global_object(fungu::const_string(cmdname,cmdname+strlen(cmdname)-1));
+    if(cmdobj)
     {
+        std::vector<fungu::script::any> args;
+        fungu::script::callargs callargs(args);
+        
+        callargs.push_back(cn);
+        
+        for(std::vector<std::string>::const_iterator it = arguments.begin() + 1; it != arguments.end(); ++it)
+            callargs.push_back(fungu::const_string(it->c_str(),it->c_str()+it->length() - 1));
+        
         try{
-            fungu::script::execute_file(cmdfile,env);
+            fungu::script::env::frame frame(&env);
+            cmdobj->call(callargs, &frame);
         }
         catch(fungu::script::error_trace * errinfo)
         {
             report_script_error(errinfo);
             server::player_msg(cn, RED "Command Error!");
         }
-    }
-    else
-    {
-        defformatstring(cmdname)("playercmd_%s",arguments[0].c_str());
-        fungu::script::env::object * cmdobj = env.lookup_global_object(fungu::const_string(cmdname,cmdname+strlen(cmdname)-1));
-        if(cmdobj)
+        catch(fungu::script::error err)
         {
-            std::vector<fungu::script::any> args;
-            fungu::script::callargs callargs(args);
-            
-            callargs.push_back(cn);
-            
-            for(std::vector<std::string>::const_iterator it = arguments.begin() + 1; it != arguments.end(); ++it)
-                callargs.push_back(fungu::const_string(it->c_str(),it->c_str()+it->length() - 1));
-            
-            try{
-                fungu::script::env::frame frame(&env);
-                cmdobj->call(callargs, &frame);
-            }
-            catch(fungu::script::error_trace * errinfo)
-            {
-                report_script_error(errinfo);
-                server::player_msg(cn, RED "Command Error!");
-            }
-            catch(fungu::script::error err)
-            {
-                report_script_error(new fungu::script::error_trace(err,"",
-                    (env.get_source_context() ? env.get_source_context()->clone() : NULL)));
-                server::player_msg(cn, RED "Command Error!");
-            }
+            report_script_error(new fungu::script::error_trace(err,"",
+                (env.get_source_context() ? env.get_source_context()->clone() : NULL)));
+            server::player_msg(cn, RED "Command Error!");
         }
-        else if(using_command_prefix) server::player_msg(cn, RED "Command not found.");
     }
+    else if(using_command_prefix) server::player_msg(cn, RED "Command not found.");
     
     return true;
 }
