ec218df5e53d8031dd9c572849d86cc9333dc0b9
asX@verfriemelt.org <asX@verfriemelt.org>
2009-09-10 20:07:47 +0000

    fixed a thing in permban cmd
    git-svn-id: http://hopmod.googlecode.com/svn/branches/ng@1105 d7818858-e1b4-11dd-a738-5fe2e3cfbcc5

--- script/command/permban.lua
+++ script/command/permban.lua
@@ -1,9 +1,9 @@
 local pb_file = io.open(server.banlist_file, "a+")
 
 local function permban(cn_ban, ip_ban)
-    pb_file:write("permban " .. ip_ban .. "\n")
-	server.kick(cn_ban, 99999999, "", red("permbanned"))
-    pb_file:flush()
+	pb_file:write("permban " .. ip_ban .. "\n")
+	server.kick(cn_ban, -1, "", red("permbanned"))
+	pb_file:flush()
 end
 
 return function(cn, cn_ban)
