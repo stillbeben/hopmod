f53c8e1ab469eb329b2123830b5dda97c5cc1aaf
graham.daws <graham.daws>
2009-09-20 20:29:00 +0000

    Made the ownage module un/re-loadable
    git-svn-id: http://hopmod.googlecode.com/svn/branches/ng@1140 d7818858-e1b4-11dd-a738-5fe2e3cfbcc5

--- script/ownage.lua
+++ script/ownage.lua
@@ -1,39 +1,5 @@
-local first_frag_events_active = 0
-local first_frag_events = {}
 
-local function first_frag_disabler()
-    if first_frag_events_active == 1 then
-	first_frag_events_active = 0
-	
-	for a,b in ipairs(first_frag_events) do
-	    server.cancel_handler(b)
-	end
-    end
-end
-
-local function first_frag_event(tcn,acn)
-    server.msg(string.format("\f2%s opened the \f6FIGHT!!",server.player_name(acn)))
-    first_frag_disabler()
-end
-
-local function first_frag_enabler()
-    if first_frag_events_active == 0 then
-	first_frag_events_active = 1
-	
-	first_frag_events = {}
-	local event_frag = server.event_handler("frag",first_frag_event)
-	table.insert(first_frag_events,event_frag)
-    end
-end
-
-server.event_handler("finishedgame",function()
-    first_frag_enabler()
-end)
-
-first_frag_enabler()
-
-
-
+-- Keep these messages at the top of the file so users can find them easily
 local killingspree_message = {}
 killingspree_message[5] = "\f2%s is on a \f6KILLING SPREE!!"
 killingspree_message[10] = "\f2%s is on a \f6RAMPAGE!!"
@@ -41,13 +7,23 @@ killingspree_message[15] = "\f2%s is \f6DOMINATING!!"
 killingspree_message[20] = "\f2%s is \f6UNSTOPPABLE!!"
 killingspree_message[30] = "\f2%s is \f6GODLIKE!!"
 
-server.event_handler("frag", function(target_cn, actor_cn)
-    local actor_vars = server.player_vars(actor_cn)
+local long_killingspree = 15
+local multikill_timelimit = 2000
+
+local first_frag = true
+
+local function send_first_frag_message(target, actor)
+    if not first_frag then return end
+    server.msg(string.format("\f2%s opened the \f6FIGHT!!", server.player_name(actor)))
+    first_frag = false
+end
+
+local function send_killingspree_message(target_cn, target_vars, actor_cn, actor_vars)
     
-    local actor_killingspree = actor_vars.killingspree
+    local actor_killingspree = actor_vars.killingspree or 0
     
     if actor_cn ~= target_cn then
-	actor_killingspree = (actor_killingspree or 0) + 1
+        actor_killingspree = actor_killingspree + 1
         actor_vars.killingspree = actor_killingspree
     else
         actor_killingspree = 0
@@ -55,53 +31,73 @@ server.event_handler("frag", function(target_cn, actor_cn)
     end
     
     if killingspree_message[actor_killingspree] then
-	actor_vars.is_killingspree = 1
-	server.msg(string.format(killingspree_message[actor_killingspree], server.player_name(actor_cn)))
+        server.msg(string.format(killingspree_message[actor_killingspree], server.player_name(actor_cn)))
     end
     
+    if target_vars.killingspree >= long_killingspree then
+        server.msg(string.format("\f2%s was stopped by \f6%s!!", server.player_name(target_cn), server.player_name(actor_cn)))
+    end
+    
+    target_vars.killingspree = 0
+end
+
+local function send_multikills_message(target_cn, target_vars, actor_cn, actor_vars)
+    
+    local lastkill = actor_vars.lastkill or 0
+    
+    if actor_cn == target_cn then
+        actor_vars.multikills = 1
+        return
+    end
+    
+    if server.gamemillis - lastkill < multikill_timelimit then
+        
+        local actor_multikills = (actor_vars.multikills or 1) + 1
+        actor_vars.multikills = actor_multikills
+        
+        if actor_multikills == 2 then
+            server.player_msg(actor_cn, yellow("You scored a ") .. orange("DOUBLE KILL!!"))
+        elseif actor_multikills == 3 then
+            server.player_msg(actor_cn, yellow("You scored a ") .. orange("TRIPLE KILL!!"))
+        elseif actor_multikills > 3 then
+            server.player_msg(actor_cn, yellow("You scored ") ..  orange(string.format("MULTPLE KILLS(%i)!!",actor_multikills)))
+        end
+        
+    else -- reset
+        actor_vars.multikills = 1
+    end
+    
+    actor_vars.lastkill = server.gamemillis
+    
+end
+
+local frag_event = server.event_handler("frag", function(target_cn, actor_cn)
+
+    send_first_frag_message(target_cn, actor_cn)
     
     local target_vars = server.player_vars(target_cn)
-    
-    if (target_vars.is_killingspree == 1) and (target_vars.killingspree > 14) then
-	server.msg(string.format("\f2%s was stopped by \f6%s!!",server.player_name(target_cn),server.player_name(actor_cn)))
-    end
-    target_vars.is_killingspree = 0
-    target_vars.killingspree = 0
-end)
-
-server.event_handler("suicide", function(cn)
-    server.player_vars(cn).killingspree = 0
-end)
-
-
-
-server.event_handler("frag", function(target_cn, actor_cn)
     local actor_vars = server.player_vars(actor_cn)
     
-    if actor_cn ~= target_cn then
-        if actor_vars.lastkill and server.gamemillis - actor_vars.lastkill < 2000 then
-            local actor_multikills = (actor_vars.multikills or 1) + 1
-            actor_vars.multikills = actor_multikills
-            
-            if actor_multikills == 2 then
-                server.player_msg(actor_cn, yellow("You scored a ") .. orange("DOUBLE KILL!!"))
-            elseif actor_multikills == 3 then
-                server.player_msg(actor_cn, yellow("You scored a ") .. orange("TRIPLE KILL!!"))
-            elseif actor_multikills > 3 then
-                server.player_msg(actor_cn, yellow("You scored ") ..  orange(string.format("MULTPLE KILLS(%i)!!",actor_multikills)))
-            end
-        else
-	    actor_vars.multikills = 1
-        end
-	
-        actor_vars.lastkill = server.gamemillis
-    else
-	actor_vars.multikills = 1
-    end
+    send_killingspree_message(target_cn, target_vars, actor_cn, actor_vars)
     
-    server.player_vars(target_cn).multikills = 1
+    send_multikills_message(target_cn, target_vars, actor_cn, actor_vars)
+    
 end)
 
-server.event_handler("suicide", function(cn)
+local finishedgame_event = server.event_handler("finishedgame",function()
+    first_frag = true
+    -- Killing-spree and Multikills player variables are automatically cleared by the server
+end)
+
+local suicide_event = server.event_handler("suicide", function(cn)
+    server.player_vars(cn).killingspree = 0
     server.player_vars(cn).multikills = 1
 end)
+
+local function unloadEventHandlers()
+    server.cancel_handler(frag_event)
+    server.cancel_handler(finishedgame_event)
+    server.cancel_handler(suicide_event)
+end
+
+return {unload = unloadEventHandlers}
