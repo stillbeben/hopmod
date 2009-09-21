80da7a722fcb98b6dff5c061a6c3549bfa9d9c6a
zombie@c-c-z.org <zombie@c-c-z.org>
2009-09-21 23:45:53 +0000

    rechecked spec_inactives
    git-svn-id: http://hopmod.googlecode.com/svn/branches/ng@1144 d7818858-e1b4-11dd-a738-5fe2e3cfbcc5

--- script/spec.inactives.lua
+++ script/spec.inactives.lua
@@ -1,78 +1,142 @@
-local spec_inactives_check_active = 0
-local spec_inactives_events_active = 0
-local spec_inactives_events = {}
-local spec_inactives_time = round((server.spec_inactives_time / 1000),0)
-local unloadEventHandlers
-local still_loaded = true
+--[[--------------------------------------------------------------------------
+--
+--    A script to let players join spectors
+--        who are dead and reach the time limit
+--    when mastermode is open
+--            
+--]]--------------------------------------------------------------------------
 
 if server.spec_inactives_check_time <= 0 then
-    server.spec_inactives_check_time = 1000
+	server.spec_inactives_check_time = 1000
 end
 
-local function spec_inactives_set_deathtime(spec_inactives_set_deathtime_cn)
-    server.player_vars(spec_inactives_set_deathtime_cn).spec_inactives_deathtime = server.player_connection_time(spec_inactives_set_deathtime_cn)
+spec_inactives = {}	-- namespace
+
+spec_inactives.time = round((server.spec_inactives_time / 1000),0)
+
+spec_inactives.check_active = false
+
+spec_inactives.eventFrag = nil
+spec_inactives.eventSuicide = nil
+
+spec_inactives.still_loaded = true
+spec_inactives.unloadEventHandlers = nil
+
+
+function spec_inactives.setDeathtime(cn)
+
+	server.player_vars(cn).spec_inactives_deathtime = server.player_connection_time(cn)
+
 end
 
-local function loadEventHandlers()
 
-	local event_frag = server.event_handler("frag",function(spec_inactives_frag_event_tcn,spec_inactives_frag_event_acn)
-		spec_inactives_set_deathtime(spec_inactives_frag_event_tcn)
-    end)
-    
-	local event_suicide = server.event_handler("suicide",function(spec_inactives_suicide_event_cn)
-		spec_inactives_set_deathtime(spec_inactives_suicide_event_cn)
-    end)
-    
-    unloadEventHandlers = function()
-    
-        server.cancel_handler(event_frag)
-        server.cancel_handler(event_suicide)
-        
-        still_loaded = false
-    end
+function spec_inactives.enable()
+
+	if not spec_inactives.eventFrag then
+		spec_inactives.eventFrag = server.event_handler("frag",function(tcn,acn)
+
+			spec_inactives.setDeathtime(tcn)
+
+		end)
+	end
+
+	if not spec_inactives.eventSuicide then
+		spec_inactives.eventSuicide = server.event_handler("suicide",function(cn)
+
+			spec_inactives.setDeathtime(cn)
+
+		end)
+	end
+
+	spec_inactives.check_active = true
+
+end
+
+
+function spec_inactives.disable()
+
+	spec_inactives.check_active = false
+
+	if spec_inactives.eventFrag then
+		server.cancel_handler(spec_inactives.eventFrag)
+		spec_inactives.eventFrag = nil
+	end
+
+	if spec_inactives.eventSuicide then
+		server.cancel_handler(spec_inactives.eventSuicide)
+		spec_inactives.eventSuicide = nil
+	end
+
+end
+
+
+function spec_inactives.loadEventHandlers()
+
+	spec_inactives.still_loaded = true
+
+	spec_inactives.enable()
+
+	if not spec_inactives.unloadEventHandlers then
+		spec_inactives.unloadEventHandlers = function()
+
+			spec_inactives.disable()
+
+			spec_inactives.still_loaded = false
+
+		end
+	end
     
 end
 
-server.event_handler("setmastermode",function(spec_inactives_setmastermode_event_old,spec_inactives_setmastermode_event_new)
-    if spec_inactives_setmastermode_event_new == "open" then
-        spec_inactives_enabler()
-    else
-        spec_inactives_disabler()
-    end
-end)
 
-server.event_handler("mapchange",function(spec_inactives_mapchange_event_map,spec_inactives_mapchange_event_mode)
-    if tonumber(server.mastermode) == 0 then
-        spec_inactives_enabler()
-    else
-        spec_inactives_disabler()
-    end
-end)
+server.event_handler("setmastermode",function(old,new)
 
-server.interval(server.spec_inactives_check_time,function()
-    
-    if not still_loaded then return -1 end
-    
-	for player in server.gplayers() do
-        
-	    local deathtime_limit = server.player_vars(cn).spec_inactives_deathtime
-        
-	    if deathtime_limit then
-            if player:status_code(cn) == 1 then
-                if player:connection_time(cn) - deathtime_limit >= spec_inactives_time then
-                
-                    player:spec()
-                    player:msg(cn,"(" .. green("Info") .. ")  you are inactive, therefore you joined spectators.")
-                    player:vars(cn).spec_inactives_deathtime = nil
-                end
-            else
-                player:vars(cn).spec_inactives_deathtime = nil
-            end
-        end
+	if new == "open" then
+ 		spec_inactives.enable()
+	else
+		spec_inactives.disable()
 	end
 
 end)
 
-loadEventHandlers()
 
-return {unload = unloadEventHandlers}
+server.event_handler("mapchange",function(map,mode)
+
+	if tonumber(server.mastermode) == 0 then
+		spec_inactives.enable()
+	else
+		spec_inactives.disable()
+	end
+
+end)
+
+
+server.interval(server.spec_inactives_check_time,function()
+    
+	if not spec_inactives.still_loaded then
+		return -1
+	end
+
+	if spec_inactives.check_active then
+		for p in server.gplayers() do
+			local deathtime = p:vars().spec_inactives_deathtime
+
+			if deathtime then
+				if p:status_code() == 1 then
+					if p:connection_time() - deathtime >= spec_inactives.time then
+						p:spec()
+						p:msg("(" .. green("Info") .. ")  you joined spectators, because you are inactive - type " .. yellow("/setmaster 0") .. " to rejoin the game.")
+						p:vars().spec_inactives_deathtime = nil
+					end
+				else
+					p:vars().spec_inactives_deathtime = nil
+				end
+			end
+		end
+	end
+
+end)
+
+spec_inactives.loadEventHandlers()
+
+return {unload = spec_inactives.unloadEventHandlers}
