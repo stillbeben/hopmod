2f05635d7f8b0c567a0084bbf109916385dbfa7a
zombie@c-c-z.org <zombie@c-c-z.org>
2009-09-22 00:47:55 +0000

    rechecked #players
    git-svn-id: http://hopmod.googlecode.com/svn/branches/ng@1149 d7818858-e1b4-11dd-a738-5fe2e3cfbcc5

--- script/command/players.lua
+++ script/command/players.lua
@@ -1,8 +1,17 @@
--- [[ Player command written by Thomas
+-- list players + player stats
+-- [[ based on a player command written by Thomas ]] --
 
-return function(source) -- list players + player stats
-    for i, cn in ipairs(server.players()) do 
-        str = "Name: " .. server.player_name(cn) .. " Frags: " .. server.player_frags(cn) .. " Deaths: " .. server.player_deaths(cn) .. " Acc: " .. server.player_accuracy (cn)
-        server.player_msg(source, str)
-    end
+
+return function(cn)
+
+	for p in server.gplayers() do
+		local acc = 0
+		local shots = p:shots()
+		if not (shots == 0) then
+			acc = round(p:hits()/p:shots(),2)
+		end
+
+		server.player_msg(cn, "Name: " .. p:name() .. " Frags: " .. p:frags() .. " Deaths: " .. p:deaths() .. " Acc: " .. acc .. "%")
+	end
+
 end
