35a69ccd7b9ff507d823d27ccad9977106f8a349
zombie@c-c-z.org <zombie@c-c-z.org>
2009-10-01 12:56:33 +0000

    fixed #admin
    git-svn-id: http://hopmod.googlecode.com/svn/branches/ng@1213 d7818858-e1b4-11dd-a738-5fe2e3cfbcc5

--- script/command/admin.lua
+++ script/command/admin.lua
@@ -1,28 +1,55 @@
--- #admin
+--[[
 
-local function admin_domainhandler(cn,name)
-    server.setadmin(cn)
-    server.msg(server.player_name(cn) .. " claimed admin as '" .. magenta(name) .. "'")
+	A player command to raise privilege to admin
+
+]]
+
+
+local events = {}
+
+local admin_domain = server.admin_domain
+
+
+local function init()
+
+	events.domain_handler = auth.add_domain_handler(admin_domain, function(cn,name)
+
+		if server.player_vars(cn).admin and server.player_vars(cn).admin == admin_domain then
+
+			server.player_vars(cn).admin = nil
+			server.setadmin(cn)
+			server.msg(server.player_displayname(cn) .. " claimed admin as '" .. magenta(name) .. "'")
+		end
+
+	end)
+
 end
 
-server.event_handler("started", function()
-    auth.add_domain_handler(server.admin_domain, function(cn,name)
-        if server.player_vars(cn).admin then
-            if server.player_vars(cn).admin == server.admin_domain then
-                server.player_vars(cn).admin = nil
-                admin_domainhandler(cn,name)
-            end
-        end
-    end)
-end)
 
-return function(cn)
+local function unload()
+
+	if events.domain_handler then
+
+		auth.cancel_domain_handler(admin_domain,events.domain_handler)
+		events.domain_handler = nil
+	end
+
+end
+
+
+local function run(cn)
 
     if server.player_priv_code(cn) > 0 then
+
         server.unsetmaster(cn)
+
     else
-        server.player_vars(cn).admin = server.admin_domain
-        auth.sendauthreq(cn,server.admin_domain)
+
+        server.player_vars(cn).admin = admin_domain
+        auth.sendauthreq(cn,admin_domain)
     end
-    
+
 end
+
+
+return {init = init,run = run,unload = unload}
