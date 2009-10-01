f14b4b02466b85ae6f429f7791f2e0fb6ea57c48
zombie@c-c-z.org <zombie@c-c-z.org>
2009-10-01 12:53:46 +0000

    fixed #invadmin
    git-svn-id: http://hopmod.googlecode.com/svn/branches/ng@1212 d7818858-e1b4-11dd-a738-5fe2e3cfbcc5

--- script/command/invadmin.lua
+++ script/command/invadmin.lua
@@ -1,40 +1,69 @@
--- #invadmin
+--[[
 
-local function invadmin_setInvadmin(cn)
+	A player command to raise privilege to (invisble) admin
+
+]]
+
+
+local events = {}
+
+local invadmin_domain = server.invadmin_domain
+
+
+local function set_invadmin(cn)
 
 	server.set_invadmin(cn)
-	server.player_msg(cn,"(" .. green("Info") .. ") Your rights have been raised to invisible-admin.")
+	server.player_msg(cn,"Your rights have been raised to invisible-admin.")
 
 end
 
 
-server.event_handler("started",function()
+local function init()
 
-	auth.add_domain_handler(server.invadmin_domain,function(cn,name)
+	events.domain_handler = auth.add_domain_handler(invadmin_domain,function(cn,name)
 
-		if server.player_vars(cn).invadmin then
-			if server.player_vars(cn).invadmin == server.invadmin_domain then
-				server.player_vars(cn).invadmin = nil
-				invadmin_setInvadmin(cn)
-			end
+		if server.player_vars(cn).invadmin and server.player_vars(cn).invadmin == invadmin_domain then
+
+			server.player_vars(cn).invadmin = nil
+			set_invadmin(cn)
 		end
 
 	end)
 
-end)
+end
 
 
-return function(cn,pw)
+local function unload()
 
-	if pw then
-		if server.check_admin_password(pw) then
-			invadmin_setInvadmin(cn)
-		end
-	elseif server.player_priv_code(cn) > 0 then
-		server.unset_invadmin(cn)
-	else
-		server.player_vars(cn).invadmin = server.invadmin_domain
-		auth.sendauthreq(cn,server.invadmin_domain)
+	if events.domain_handler then
+
+		auth.cancel_domain_handler(invadmin_domain,events.domain_handler)
+		events.domain_handler = nil
 	end
 
 end
+
+
+local function run(cn,pw)
+
+	if pw then
+
+		if server.check_admin_password(pw) then
+
+			set_invadmin(cn)
+		end
+
+	elseif server.player_priv_code(cn) > 0 then
+
+		server.unset_invadmin(cn)
+
+	else
+
+		server.player_vars(cn).invadmin = invadmin_domain
+		auth.sendauthreq(cn,invadmin_domain)
+	end
+
+end
+
+
+return {init = init,run = run,unload = unload}
