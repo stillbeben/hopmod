13a72ca173bb0ebd1c8b709139e131342ae5d5bb
zombie@c-c-z.org <zombie@c-c-z.org>
2009-10-01 12:51:31 +0000

    added name-support to #names
    git-svn-id: http://hopmod.googlecode.com/svn/branches/ng@1209 d7818858-e1b4-11dd-a738-5fe2e3cfbcc5

--- script/command/names.lua
+++ script/command/names.lua
@@ -1,15 +1,36 @@
--- #names <cn>
+--[[
+
+	A player command to list all known names to player
+	based on the stats.db and player_ip
+
+]]
+
 
 return function(cn, target_cn)
-    if server.valid_cn(target_cn) then
+
+	if not target_cn then
+
+		return false, "#names <cn>|\"<name>\""
+	end
+
+	if not server.valid_cn(target_cn) then
+
+		target_cn = server.name_to_cn_list_matches(cn,target_cn)
+
+		if not target_cn then
+
+			return
+		end
+	end
+
 	local db = sqlite3.open(server.stats_db_filename)
-        local str = "Other names used by " .. server.player_name(target_cn) .. ": "
-        for name, count in db:cols("SELECT DISTINCT name, count(name) as count FROM players WHERE ipaddr = '" .. server.player_ip(target_cn) .. "'") do
-            str = str .. name .. "(" .. count .. "),"
-        end
-        server.player_msg(cn, str)
-        db:close(db)
-    else
-        server.player_msg(cn,red("cn is not valid"))
-    end
+
+	local str = "Other names used by " .. server.player_name(target_cn) .. ": "
+	for name, count in db:cols("SELECT DISTINCT name, count(name) as count FROM players WHERE ipaddr = '" .. server.player_ip(target_cn) .. "'") do
+		str = str .. name .. "(" .. count .. "),"
+	end
+	server.player_msg(cn, str)
+
+	db:close()
+
 end
