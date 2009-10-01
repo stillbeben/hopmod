62cc59d3e88de09098ec99e56b6eb6adbfd03a52
zombie@c-c-z.org <zombie@c-c-z.org>
2009-10-01 12:45:38 +0000

    added name-support to #unmute
    git-svn-id: http://hopmod.googlecode.com/svn/branches/ng@1205 d7818858-e1b4-11dd-a738-5fe2e3cfbcc5

--- script/command/unmute.lua
+++ script/command/unmute.lua
@@ -1,10 +1,27 @@
+--[[
 
-return function(unmute_cn, unmute_tcn)
-    if not unmute_tcn then
-        server.player_msg(unmute_cn,red("#unmute <cn>"))
-    elseif not server.valid_cn(unmute_tcn) then
-        server.player_msg(unmute_cn,red("cn is not valid"))
-    else
-        server.unmute(unmute_tcn)
-    end
+	A player command to unmute a player
+
+]]
+
+
+return function(cn,tcn)
+
+	if not tcn then
+
+		return false, "#unmute <cn>|\"<name>\""
+	end
+
+	if not server.valid_cn(tcn) then
+
+		tcn = server.name_to_cn_list_matches(cn,tcn)
+
+		if not tcn then
+
+			return
+		end
+	end
+
+	server.unmute(tcn)
+
 end
