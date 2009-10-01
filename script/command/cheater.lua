42f7f3501dae2e8c3fc09e2cd13f05c6662b236c
zombie@c-c-z.org <zombie@c-c-z.org>
2009-10-01 12:57:37 +0000

    fixed #cheater
    git-svn-id: http://hopmod.googlecode.com/svn/branches/ng@1214 d7818858-e1b4-11dd-a738-5fe2e3cfbcc5

--- script/command/cheater.lua
+++ script/command/cheater.lua
@@ -1,54 +1,97 @@
--- #cheater <cn>|"<name>"
+--[[
 
--- TODO somehow cancel this handler when the command is disabled
-server.event_handler("started",function()
-    if server.cheater_ad_timer ~= 0 then
-	server.interval(server.cheater_ad_timer,function()
-	    if (tonumber(server.playercount) > 2) and (tonumber(server.mastermode) == 0) then
-		server.msg("(" .. green("Info") .. ")  " .. "If you suspect a cheater type " .. yellow("#cheater \"name\"") .. " or " .. yellow("#cheater cn") .. " to notify an admin.")
-	    end
-	end)
-    end
-end)
+	A player command to send a name of a found cheater to the log
 
-local function usage()
-    return "#cheater <CN> or <NAME>"
-end
+]]
 
-return function(cn, cheat)
 
-    if not cheat then
-        return false, usage()
-    end
-    
-    if not server.valid_cn(cheat) then
-        cheat = server.name_to_cn(cheat)
-        if not cheat then
-            return false, usage()
-        end
-    end
-    
-    cheat = tonumber(cheat)
-    if server.player_sessionid(cheat) ~= -1 then
-	local cheat_report = server.player_pvars(cn).cheat_report
-    
-	if not cheat_report then
-	    cheat_report = 0
+local events = {}
+local interval = {}
+interval.timer = server.cheater_ad_timer
+
+local usage = "#cheater <cn>|\"<name>\""
+
+
+local function init()
+
+	if not (interval.timer == 0) then
+
+		interval.active = true
+
+		server.interval(interval.timer,function()
+
+			if interval.active == false then
+
+				return -1
+			end
+
+			if (tonumber(server.playercount) > 2) and (tonumber(server.mastermode) == 0) then
+
+				server.msg("If you suspect a cheater type " .. yellow("#cheater \"name\"") .. " or " .. yellow("#cheater CN") .. " to notify an admin.")
+			end
+
+		end)
+
 	end
-    
-	cheat_report = cheat_report + 1
-    server.player_pvars(cn).cheat_report = cheat_report
-    
-    if cheat_report > 4 then
-        server.player_msg(cn,"Don't spam with the #cheater command or you will be ignored.")
-    end
-    
-    if cheat_report < 8 then
-        if server.player_connection_time(cheat) > 3 then
-            server.player_msg(cn,"Thank you for your report, hopefully an admin will check this out very soon.")
-            server.log("CHEATER: " .. server.player_name(cheat) .. " was reported by " .. server.player_name(cn))
+
+end
+
+
+local function unload()
+
+	interval.active = false
+
+end
+
+
+local function run(cn,cheat)
+
+	if not cheat then
+
+		return false, usage
+	end
+
+	if not server.valid_cn(cheat) then
+
+		cheat = server.name_to_cn_list_matches(cn,cheat)
+
+		if not cheat then
+
+			return
+
+		end
+	end
+
+	cheat = tonumber(cheat)
+	cn = tonumber(cn)
+	
+	if cheat == cn then
+
+		return false, "You can't report yourself"
+	end
+
+	if not server.player_vars(cn).cheater then
+
+		server.player_vars(cn).cheater = {}
+	end
+
+	local cheat_report = (server.player_vars(cn).cheater[cheat] or 0) + 1
+	server.player_vars(cn).cheater[cheat] = cheat_report
+
+	if cheat_report > 4 then
+
+		server.player_msg(cn,orange("Don't spam with the #cheater command or you will be ignored."))
+	end
+
+	if cheat_report < 8 then
+
+		if server.player_connection_time(cheat) > 10 then
+
+			server.player_msg(cn,"Thank you for your report, hopefully an admin will check this out very soon.")
+			server.log("CHEATER: " .. server.player_name(cheat) .. "(" .. cheat .. ") was reported by " .. server.player_name(cn) .. "(" .. cn .. ")")
         end
     end
-    
+
 end
-end
+
+return {init = init,run = run,unload = unload}
