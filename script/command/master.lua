22a9391bb1b6ad7c89f08d26d80fec6ca47eb6e3
zombie@c-c-z.org <zombie@c-c-z.org>
2009-10-01 12:52:37 +0000

    fixed #master
    git-svn-id: http://hopmod.googlecode.com/svn/branches/ng@1211 d7818858-e1b4-11dd-a738-5fe2e3cfbcc5

--- script/command/master.lua
+++ script/command/master.lua
@@ -1,34 +1,68 @@
--- #master <domain>
+--[[
+
+	A player command to raise privilege to master
+
+]]
+
+
+local events = {}
 
 local master_domains = table_unique(server.parse_list(server["master_domains"]))
 
-local function master_domainhandler(cn,name)
-    server.setmaster(cn)
-    server.msg(server.player_name(cn) .. " claimed master as '" .. magenta(name) .. "'")
+
+local function init()
+
+	for i,domain in ipairs(master_domains) do
+
+		events[domain] = auth.add_domain_handler(domain,function(cn,name)
+
+			if server.player_vars(cn).master and server.player_vars(cn).master == domain then
+
+				server.player_vars(cn).master = nil
+				server.setmaster(cn)
+				server.msg(server.player_displayname(cn) .. " claimed master as '" .. magenta(name) .. "'")
+			end
+
+		end)
+
+	end
+
 end
 
-server.event_handler("started",function()
-    for a,b in ipairs(master_domains) do
-        auth.add_domain_handler(b,function(cn,name)
-            if server.player_vars(cn).master then
-                if server.player_vars(cn).master == b then
-                    server.player_vars(cn).master = nil
-                    master_domainhandler(cn,name)
-                end
-            end
-        end)
-    end
-end)
 
-return function(cn, master_domain)
-    if server.player_priv_code(cn) > 0 then
-        server.unsetmaster(cn)
-    else
-        for a,b in ipairs(master_domains) do
-            if master_domain == b then
-                server.player_vars(cn).master = b
-                auth.sendauthreq(cn,b)
-            end
-        end
-    end
+local function unload()
+
+	for i,domain in ipairs(master_domains) do
+
+		if events[domain] then
+
+			auth.cancel_domain_handler(domain,events[domain])
+			events[domain] = nil
+		end
+	end
+
 end
+
+
+local function run(cn,master_domain)
+
+	if server.player_priv_code(cn) > 0 then
+
+		server.unsetmaster(cn)
+
+	else
+
+		for i,domain in ipairs(master_domains) do
+
+			if master_domain == domain then
+
+				server.player_vars(cn).master = domain
+				auth.sendauthreq(cn,domain)
+				break
+			end
+		end
+	end
+
+end
+
+return {init = init, run = run, unload = unload}
