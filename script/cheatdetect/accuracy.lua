9108e51d2868443a6bf33017b65056b8b4aaad01
zombie@c-c-z.org <zombie@c-c-z.org>
2009-10-01 12:36:35 +0000

    fixed accuracy logging
    git-svn-id: http://hopmod.googlecode.com/svn/branches/ng@1200 d7818858-e1b4-11dd-a738-5fe2e3cfbcc5

--- script/cheatdetect/accuracy.lua
+++ script/cheatdetect/accuracy.lua
@@ -2,16 +2,29 @@ local min_played_time = 180
 local min_frags = 15
 
 
-server.event_handler("finishedgame",function()
+local events = {}
 
-	for p in server.gplayers() do
-		local acc = server.player_acc(p.cn)
+
+events.finishedgame = server.event_handler_object("finishedgame",function()
+
+	for p in server.aplayers() do
+		local acc = p:accuracy()
 		local time = p:timeplayed()
-		local frags = p:frags()
+		local frags = p:frags() + p:teamkills() + p:suicides()
 
 		if (frags > min_frags) and (time > min_played_time) and (acc > server.cd_accuracy_limit) then
-			server.log("WARNING: " .. p:name() .. "(" .. p.cn .. ") had an accuracy of: " .. acc .. "%. [frags/deaths: " .. ((frags + p:teamkills()) + p:suicides()) .. "/" .. p:deaths() .. " | ip: " .. p:ip() .. "]")
+			server.log("WARNING: " .. p:name() .. "(" .. p.cn .. ") had an accuracy of: " .. acc .. "%. [frags/deaths: " .. frags .. "/" .. p:deaths() .. " | ip: " .. p:ip() .. "]")
 		end
 	end
 
 end)
+
+
+local function disable()
+
+	events = nil
+
+end
+
+
+return {unload = disable}
