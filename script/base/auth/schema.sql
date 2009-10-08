c434472a625ff3b7e68eaf3fc0422c7548c7bd85
graham.daws <graham.daws>
2009-10-08 22:00:48 +0000

    Added servers table to the auth schema.
    git-svn-id: http://hopmod.googlecode.com/svn/branches/ng@1231 d7818858-e1b4-11dd-a738-5fe2e3cfbcc5

--- script/base/auth/schema.sql
+++ script/base/auth/schema.sql
@@ -1,11 +1,19 @@
 
-CREATE TABLE domains (
+CREATE TABLE IF NOT EXISTS servers (
     id          INTEGER PRIMARY KEY,
+    servername  TEXT UNIQUE,
+    hostname    TEXT,
+    port        INTEGER DEFAULT 28787
+);
+
+CREATE TABLE IF NOT EXISTS domains (
+    id          INTEGER PRIMARY KEY,
+    server_id   INTEGER REFERENCES servers(id),
     name        TEXT UNIQUE,
     local       BOOLEAN DEFAULT 0
 );
 
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
     id          INTEGER PRIMARY KEY,
     domain_id   INTEGER REFERENCES domains(id),
     name        TEXT UNIQUE,
