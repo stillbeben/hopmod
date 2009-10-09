ce566d68126291a4da78bb5ee55ac449c586398a
graham.daws <graham.daws>
2009-10-09 20:29:58 +0000

    Added table.deepcopy function
    git-svn-id: http://hopmod.googlecode.com/svn/branches/ng@1235 d7818858-e1b4-11dd-a738-5fe2e3cfbcc5

--- script/base/utils.lua
+++ script/base/utils.lua
@@ -443,3 +443,22 @@ function validate_table(subject_table, schema)
     end
     
 end
+
+-- Copied from the lua-users wiki
+function table.deepcopy(object)
+    local lookup_table = {}
+    local function _copy(object)
+        if type(object) ~= "table" then
+            return object
+        elseif lookup_table[object] then
+            return lookup_table[object]
+        end
+        local new_table = {}
+        lookup_table[object] = new_table
+        for index, value in pairs(object) do
+            new_table[_copy(index)] = _copy(value)
+        end
+        return setmetatable(new_table, getmetatable(object))
+    end
+    return _copy(object)
+end
