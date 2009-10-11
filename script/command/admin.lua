ad9b6ffa5220d02aefb7c186066bedd87111e086
graham.daws <graham.daws>
2009-10-11 12:50:52 +0000

    #admin command improvements: Give error message to player on failure and write success messages to the server log.
    git-svn-id: http://hopmod.googlecode.com/svn/branches/ng@1242 d7818858-e1b4-11dd-a738-5fe2e3cfbcc5

--- script/command/admin.lua
+++ script/command/admin.lua
@@ -1,6 +1,10 @@
 --[[
 
 	A player command to raise privilege to admin using auth
+    
+    WARNING: Enabling the admin command with the admin_domain variable set
+    to "" means players with a master key can gain admin privilege on your
+    server. TODO Check for this?
 ]]
 
 local admin_domain = server.admin_domain or ""
@@ -9,14 +13,22 @@ local function init() end
 local function unload() end
 
 local function run(cn)
-
+    
+    local session_id = server.player_sessionid(cn)
+    
     auth.send_request(cn, admin_domain, function(cn, user_id, domain, status)
         
-        if status ~= auth.request_status.SUCCESS then return end
-    
-        server.setadmin(cn)
-        server.msg(server.player_displayname(cn) .. " claimed admin as '" .. magenta(user_id) .. "'")
+        if session_id ~= server.player_sessionid(cn) then return end
         
+        if status ~= auth.request_status.SUCCESS then
+            server.player_msg(cn, red("Admin command failed: unable to authenticate"))
+            return 
+        end
+        
+        server.setadmin(cn)
+        
+        server.msg(server.player_displayname(cn) .. " claimed admin as '" .. magenta(user_id) .. "'")
+        server.log(string.format("%s playing as %s(%i) used auth to claim admin.", user_id, server.name(cn), cn))
     end)
     
 end
