d17b74e451f35f03a4e1b30988e3d34aaa3c5625
graham.daws <graham.daws>
2009-10-25 22:31:08 +0000

    Added seconds as an optional parameter for the #changetime command.
    git-svn-id: http://hopmod.googlecode.com/svn/branches/ng@1288 d7818858-e1b4-11dd-a738-5fe2e3cfbcc5

--- script/command/changetime.lua
+++ script/command/changetime.lua
@@ -1,26 +1,21 @@
 --[[
-
 	A player command to change the current map time
-
 ]]
 
+return function(cn,minutes, seconds)
 
-return function(cn,time)
-
-	if not time then
-
-		return false, "#changetime <time>"
+	if not minutes then
+		return false, "#changetime <minutes> [<seconds>]"
 	end
-
-	time = tonumber(time)
-
-	if time >= 0 and time < 13670 then
-
-		server.changetime(((time*60)*1000))
-
-       else
-
-		return false, "max map.time is 13669"
-	end
-
+    
+	minutes = tonumber(time)
+    seconds = tonumber(time or 0)
+    
+	--if minutes >= 0 and time < 13670 then
+    --    server.changetime(((time*60)*1000))
+    --else
+	--	return false, "max map.time is 13669"
+	--end
+    
+    server.changetime((minutes*60*1000)+seconds*1000)
 end
