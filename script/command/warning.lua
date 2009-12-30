aac91d4aa11047a44471100f897c0709fb38088e
zombie@c-c-z.org <zombie@c-c-z.org>
2009-12-30 19:08:59 +0000

    reviewed #warning
    git-svn-id: http://hopmod.googlecode.com/svn/branches/ng@1433 d7818858-e1b4-11dd-a738-5fe2e3cfbcc5

--- script/command/warning.lua
+++ script/command/warning.lua
@@ -9,27 +9,36 @@
 local limit = server.warning_limit
 local bantime = round(server.warning_bantime / 1000,0)
 
-local usage = "warning (<cn>|\"<name>\") \"<text>\""
+local usage = "warning (<cn>|\"<name>\") <text>"
 
 
-return function(cn,tcn,text)
+return function(cn, tcn, ...)
 
 	if not tcn then
-
 		return false, usage
 	end
 
-	if not text then
+	local text = ""
 
-		return false, usage
+	for _, item in ipairs(arg) do
+		item = tostring(item)
+		if #item > 0 then
+			if #text > 0 then
+				text = text .. " "
+			end
+
+			text = text .. item
+		end
+	end
+
+	if text == "tk" then
+		text = "Stop teamkilling. ONLY RED players are the enemies!"
 	end
 
 	if not server.valid_cn(tcn) then
-
 		tcn = server.name_to_cn_list_matches(cn,tcn)
 
 		if not tcn then
-
 			return
 		end
 	end
@@ -37,11 +46,9 @@ return function(cn,tcn,text)
 	local warn_count = (server.player_vars(tcn).warning_count or 1)
 
 	if warn_count <= limit then
-
 		local msg = "Warning"
 
-		if warn_count == limit then
-
+		if warn_count == limit and limit > 1 then
 			msg = "Last " .. msg
 		end
 
@@ -50,10 +57,8 @@ return function(cn,tcn,text)
 		server.player_msg(tcn," ")
 
 		server.player_vars(tcn).warning_count = warn_count + 1
-
 	else
-
-		server.kick(tcn,bantime,server.player_name(cn),"")
+		server.kick(tcn,bantime,server.player_name(cn),"warning limit reached")
 		server.player_vars(tcn).warning_count = nil
 	end
 
