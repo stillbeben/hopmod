a491598940450414a950ab912c9ee2fde0baab59
zombie@c-c-z.org <zombie@c-c-z.org>
2009-12-30 20:37:10 +0000

    reviewed cheatdetect/chainsawhack [still PoC]
    git-svn-id: http://hopmod.googlecode.com/svn/branches/ng@1454 d7818858-e1b4-11dd-a738-5fe2e3cfbcc5

--- script/cheatdetect/chainsawhack.lua
+++ script/cheatdetect/chainsawhack.lua
@@ -1,25 +1,28 @@
-local events = {}
-
-events.frag = server.event_handler_object("frag",function(tcn,acn)
+local event_frag = server.event_handler_object("frag",function(tcn,acn)
 
 	local a_pos_x,a_pos_y,a_pos_z = server.player_pos(acn)
 	local t_pos_x,t_pos_y,t_pos_z = server.player_pos(tcn)
 	local weapon = server.player_gun(acn)
 
 	if weapon == 0 then
-		if (math.abs(a_pos_x - t_pos_x) > 24) or (math.abs(a_pos_y - t_pos_y) > 24) or (math.abs(a_pos_z - t_pos_z) > 24) then
+
+		-- orig: > 20
+		if (math.abs(a_pos_x - t_pos_x) > 26) or (math.abs(a_pos_y - t_pos_y) > 26) or (math.abs(a_pos_z - t_pos_z) > 26) then
+
 			server.log("WARNING: " .. server.player_name(acn) .. "(" .. acn .. ") uses a chainsaw hack.  [pj: " .. server.player_lag(acn) .. " | ping: " .. server.player_ping(acn) .. " | ip: " .. server.player_ip(acn) .. "]")
+
 		end
+
 	end
 
 end)
 
 
-local function disable()
+local function unload()
 
-	events = nil
+	event_frag = nil
 
 end
 
 
-return {unload = disable}
+return {unload = unload}
