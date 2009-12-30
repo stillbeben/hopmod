377741242b042ca53762c8a0a658c6cc5de0df99
zombie@c-c-z.org <zombie@c-c-z.org>
2009-12-30 20:12:21 +0000

    reviewed #admin
    git-svn-id: http://hopmod.googlecode.com/svn/branches/ng@1450 d7818858-e1b4-11dd-a738-5fe2e3cfbcc5

--- script/command/admin.lua
+++ script/command/admin.lua
@@ -7,30 +7,44 @@
     server. TODO Check for this?
 ]]
 
-local admin_domain = server.admin_domain or ""
+
+local admin_domains = table_unique(server.parse_list(server["admin_domains"])) or ""
+
 
 local function init() end
+
 local function unload() end
 
 local function run(cn)
     
-    local session_id = server.player_sessionid(cn)
+	if server.player_priv_code(cn) > 0 then
+
+		server.unsetmaster(cn)
+
+	else
+
+		local session_id = server.player_sessionid(cn)
+
+		for i,admin_domain in ipairs(admin_domains) do
+
+			auth.send_request(cn, admin_domain, function(cn, user_id, domain, status)
+
+				if session_id ~= server.player_sessionid(cn) or status ~= auth.request_status.SUCCESS then
+					return 
+				end
+        
+				server.setadmin(cn)
+        
+				server.msg(server.player_displayname(cn) .. " claimed admin as '" .. magenta(user_id) .. "'")
+				server.log(string.format("%s playing as %s(%i) used auth to claim admin.", user_id, server.player_name(cn), cn))
+	
+			end)
+
+		end
     
-    auth.send_request(cn, admin_domain, function(cn, user_id, domain, status)
-        
-        if session_id ~= server.player_sessionid(cn) then return end
-        
-        if status ~= auth.request_status.SUCCESS then
-            server.player_msg(cn, red("Admin command failed: unable to authenticate"))
-            return 
-        end
-        
-        server.setadmin(cn)
-        
-        server.msg(server.player_displayname(cn) .. " claimed admin as '" .. magenta(user_id) .. "'")
-        server.log(string.format("%s playing as %s(%i) used auth to claim admin.", user_id, server.player_name(cn), cn))
-    end)
-    
+	end
+
 end
+
 
 return {init = init,run = run,unload = unload}
