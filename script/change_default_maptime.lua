72ac808bcf32be669e3a1ed2fb45ba0e3092a4e7
zombie@c-c-z.org <zombie@c-c-z.org>
2009-12-30 17:48:49 +0000

    reviewed change_default_maptime
    git-svn-id: http://hopmod.googlecode.com/svn/branches/ng@1412 d7818858-e1b4-11dd-a738-5fe2e3cfbcc5

--- script/change_default_maptime.lua
+++ script/change_default_maptime.lua
@@ -1,5 +1,27 @@
-server.changetime(server.default_maptime)
 
-server.event_handler("mapchange",function(map,mode)
-    server.changetime(server.default_maptime)
+local map_time = server.default_maptime
+
+local event = {}
+
+
+event.mapchange = server.event_handler_object("mapchange", function(map, mode)
+
+	server.changetime(map_time)
+
 end)
+
+event.started = server.event_handler_object("started", function()
+
+	server.changetime(map_time)
+
+end)
+
+
+local function unload()
+
+	event = {}
+
+end
+
+
+return {unload = unload}
