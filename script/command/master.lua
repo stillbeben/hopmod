47172a2bdda254cdb02ec6b9f18a72a6426eb941
zombie@c-c-z.org <zombie@c-c-z.org>
2009-12-30 19:35:58 +0000

    fixed #master
    git-svn-id: http://hopmod.googlecode.com/svn/branches/ng@1441 d7818858-e1b4-11dd-a738-5fe2e3cfbcc5

--- script/command/master.lua
+++ script/command/master.lua
@@ -1,28 +1,54 @@
+--[[
 
-local master_domain = server.master_domain or ""
+	A player command to raise privilege to master
+
+]]
+
+
+
+local master_domains = table_unique(server.parse_list(server["master_domains"])) or ""
+
 
 local function init() end
+
 local function unload() end
 
 local function run(cn)
-    
-    local session_id = server.player_sessionid(cn)
-    
-    auth.send_request(cn, master_domain, function(cn, user_id, domain, status)
-        
-        if session_id ~= server.player_sessionid(cn) then return end
-        
-        if status ~= auth.request_status.SUCCESS then
-            server.player_msg(cn, red("Master command failed: unable to authenticate"))
-            return 
-        end
-        
-        server.setmaster(cn)
-        
-        server.msg(server.player_displayname(cn) .. " claimed master as '" .. magenta(user_id) .. "'")
-        server.log(string.format("%s playing as %s(%i) used auth to claim master.", user_id, server.player_name(cn), cn))
-    end)
-    
+
+	if server.player_priv_code(cn) > 0 then
+		server.unsetmaster(cn)
+	else
+		local session_id = server.player_sessionid(cn)
+
+		for i,master_domain in ipairs(master_domains) do
+
+			auth.send_request(cn, master_domain, function(cn, user_id, domain, status)
+
+				if session_id ~= server.player_sessionid(cn) or status ~= auth.request_status.SUCCESS then
+					return
+				end
+
+				local admin_present = server.master ~= -1 and server.player_priv_code(server.master) == server.PRIV_ADMIN
+
+				if server.player_priv_code(cn) == 0 and not admin_present and server.use_master_auth == 1 then
+
+					server.setmaster(cn)
+					server.msg(server.player_displayname(cn) .. " claimed master as '" .. magenta(user_id) .. "'")
+					server.log(string.format("%s playing as %s(%i) used auth to claim master.", user_id, server.player_name(cn), cn))
+
+				else
+
+					server.player_msg(cn,red("An admin or master is already there."))
+
+				end
+
+			end)
+
+		end
+
+	end
+
 end
 
-return {init = init,run = run,unload = unload}
+
+return {init = init, run = run, unload = unload}
