9cd28115adac0cb85f32e7191db972ac38aa5b26
graham.daws <graham.daws>
2010-02-23 21:15:03 +0000

    Include address in the create host failure message
    git-svn-id: http://hopmod.googlecode.com/svn/branches/ng@1603 d7818858-e1b4-11dd-a738-5fe2e3cfbcc5

--- src/engine/server.cpp
+++ src/engine/server.cpp
@@ -946,7 +946,11 @@ bool setuplistenserver(bool dedicated)
     
     int slots = min(maxclients + server::reserveclients(), MAXCLIENTS);
     serverhost = enet_host_create(&address, slots, 0, uprate);
-    if(!serverhost) return servererror(dedicated, "could not create server host");
+    if(!serverhost) 
+    {
+        defformatstring(errmsg)("could not create server host (%s:%i)", serverip, serverport);
+        return servererror(dedicated, errmsg);
+    }
     loopi(slots) serverhost->peers[i].data = NULL;
     const char * _serverip = serverip[0] == '\0' ? "0.0.0.0" : serverip;
     std::cout<<"Game server socket listening on UDP "<<_serverip<<":"<<serverport<<std::endl;
