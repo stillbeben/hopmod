26e21a64cc1efb371fbdaa68b22ff3fc887f9f1b
graham.daws <graham.daws>
2010-05-10 23:31:57 +0000

    Removed servername line from server banner
    git-svn-id: http://hopmod.googlecode.com/svn/trunk@1735 d7818858-e1b4-11dd-a738-5fe2e3cfbcc5

--- script/base/banner.lua
+++ script/base/banner.lua
@@ -14,9 +14,7 @@ local function sendServerBanner(cn)
         -- cancel if not the same player from 1 second ago
         if sid ~= server.player_sessionid(cn) then return end
         
-        server.player_msg(cn, orange(server.servername))
         server.player_msg(cn, server.motd)
-        
         server.player_vars(cn).shown_banner = true
     end)
 end
