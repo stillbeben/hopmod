b3219502a05efd05ad697bdf1d51f911b834b69c
graham.daws <graham.daws>
2010-05-24 22:15:30 +0000

    Updated map rotation variables
    git-svn-id: http://hopmod.googlecode.com/svn/trunk@1756 d7818858-e1b4-11dd-a738-5fe2e3cfbcc5

--- script/base/maprotation/maps.cs
+++ script/base/maprotation/maps.cs
@@ -1,72 +1,117 @@
-
-ffa_maps [
+global ffa_maps [
     complex douze ot academy metl2 metl3 justice turbine mbt2 fanatic_quake dock curvy_castle duel8 nmp8 tartech aard3c industry
     duel7 oddworld wake5 park refuge curvedm kalking1 hog2 kffa fragplaza pgdm neondevastation memento neonpanic shindou sdm1 island
     aqueducts konkuri-to moonlite darkdeath castle_trap orion katrez_d thor frostbyte ogrosupply kmap5 thetowers guacamole tejen hades paradigm wdcd
     stemple corruption lostinspace
 ]
 
-small_ffa_maps [
+global small_ffa_maps [
     complex douze ot academy metl2 metl3 justice turbine dock curvy_castle duel8 nmp8 tartech aard3c
     duel7 oddworld wake5 hog2 kffa memento neonpanic sdm1 island
 ]
 
-big_ffa_maps [
+global big_ffa_maps [
     fanatic_quake dock duel8 nmp8 industry
     wake5 park refuge curvedm kalking1 fragplaza pgdm neondevastation memento neonpanic shindou island
     aqueducts konkuri-to moonlite darkdeath castle_trap orion thetowers hades paradigm wdcd
     stemple corruption lostinspace
 ]
 
-instagib_maps [
+global instagib_maps [
     complex douze ot academy metl2 metl3 justice turbine mbt2 fanatic_quake dock curvy_castle duel8 nmp8 tartech aard3c industry
     duel7 oddworld wake5 park refuge curvedm kalking1 hog2 kffa fragplaza pgdm neondevastation memento neonpanic shindou sdm1 island
     aqueducts konkuri-to moonlite darkdeath castle_trap orion katrez_d thor frostbyte ogrosupply kmap5 thetowers guacamole tejen hades paradigm wdcd
     stemple corruption lostinspace
 ]
 
-small_instagib_maps [
+global small_instagib_maps [
     complex douze ot academy metl2 metl3 justice turbine dock curvy_castle duel8 nmp8 tartech aard3c
     duel7 oddworld wake5 hog2 kffa memento neonpanic sdm1 island
 ]
 
-big_instagib_maps [
+global big_instagib_maps [
     fanatic_quake dock duel8 nmp8 industry
     wake5 park refuge curvedm kalking1 fragplaza pgdm neondevastation memento neonpanic island
     konkuri-to moonlite darkdeath castle_trap orion thetowers paradigm wdcd
     stemple corruption lostinspace
 ]
 
-capture_maps [
+global capture_maps [
     urban_c nevil_c fb_capture nmp9 c_valley lostinspace fc3 face-capture nmp4 nmp8 hallo tempest monastery ph-capture hades fc4 relic
     venice paradigm corruption asteroids ogrosupply reissen akroseum duomo frostbyte c_egypt caribbean dust2 campo killcore3 damnation arabic cwcastle
     mercury core_transfer forge tejen capture_night river_c serenity
 ]
 
-small_capture_maps [
+global small_capture_maps [
     nmp9 fc3 face-capture nmp8 hades
     paradigm corruption asteroids reissen akroseum frostbyte caribbean dust2 campo killcore3 damnation arabic
     tejen capture_night
 ]
 
-big_capture_maps [
+global big_capture_maps [
     urban_c nevil_c fb_capture nmp4 c_valley lostinspace hallo tempest monastery ph-capture hades fc4 relic
     venice paradigm corruption ogrosupply reissen akroseum duomo c_egypt caribbean dust2 campo killcore3 damnation arabic cwcastle
     mercury core_transfer forge tejen capture_night river_c serenity
 ]
 
-ctf_maps [
+global ctf_maps [
     hallo flagstone tempest capture_night reissen tejen shipwreck authentic urban_c bt_falls l_ctf face-capture valhalla mbt1 mach2 dust2 berlin_wall
     mercury akroseum europium redemption damnation forge campo wdcd desecration sacrifice core_transfer recovery
 ]
 
-small_ctf_maps [
+global small_ctf_maps [
     flagstone tempest capture_night reissen tejen shipwreck l_ctf face-capture mbt1 mach2 dust2
     akroseum europium redemption damnation forge campo recovery
 ]
 
-big_ctf_maps [
+global big_ctf_maps [
     hallo flagstone tempest capture_night reissen tejen shipwreck authentic urban_c bt_falls l_ctf face-capture valhalla mbt1 mach2 dust2 berlin_wall
     mercury akroseum europium redemption damnation forge campo wdcd desecration sacrifice core_transfer recovery
 ]
+
+global "coop edit_maps" &ffa_maps
+global "small_coop edit_maps" &small_ffa_maps
+global "big_coop edit_maps" &big_ffa_maps
+
+global "teamplay_maps" &ffa_maps
+global small_teamplay_maps &small_ffa_maps
+global big_teamplay_maps &big_ffa_maps
+
+global "efficiency_maps" &ffa_maps
+global small_efficiency_maps &small_ffa_maps
+global big_efficiency_maps &big_ffa_maps
+
+global "efficiency team_maps" &ffa_maps
+global "small_efficiency team_maps" &small_ffa_maps
+global "big_efficiency team_maps" &big_ffa_maps
+
+global "tactics_maps" &ffa_maps
+global small_tactics_maps &small_ffa_maps
+global big_tactics_maps &big_ffa_maps
+
+global "tactics team_maps" &ffa_maps
+global "small_tactics team_maps" &small_ffa_maps
+global "big_tactics team_maps" &big_ffa_maps
+
+global "instagib team_maps" &instagib_maps
+global "small_instagib team_maps" &small_instagib_maps
+global "big_instagib team_maps" &big_instagib_maps
+
+global "regen capture_maps" &capture_maps
+global "small_regen capture_maps" &small_capture_maps
+global "big_regen capture_maps" &big_capture_maps
+
+global "insta ctf_maps" &ctf_maps
+global "small_insta ctf_maps" &small_ctf_maps
+global "big_insta ctf_maps" &big_ctf_maps
+
+global "protect_maps" &ctf_maps
+global small_protect_maps &small_ctf_maps
+global big_protect_maps &big_ctf_maps
+
+global "insta protect_maps" &ctf_maps
+global "small_insta protect_maps" &small_ctf_maps
+global "big_insta protect_maps" &small_ctf_maps
+
+
 
--- script/base/resetvars.cs
+++ script/base/resetvars.cs
@@ -30,133 +30,13 @@ global allowed_gamemodes [
 global use_server_maprotation 1
 global map_rotation_type "standard"
 
-global use_best_map_size 0
 global small_gamesize 5
 global small_teamgamesize 5
 
+exec "script/base/maprotation/maps.cs"
+
 global default_gamemode "ffa"
 global default_game_on_empty 0
-
-global ffa_maps [complex douze ot academy metl2 metl3 nmp8 refuge tartech
-	kalking1 dock turbine fanatic_quake oddworld wake5 aard3c curvedm
-	fragplaza pgdm kffa neondevastation hog2 memento neonpanic lostinspace
-	DM_BS1 shindou sdm1 shinmei1 stemple powerplant phosgene oasis island
-	metl4 ruby frozen ksauer1 killfactory corruption deathtek aqueducts orbe
-	arabic roughinery shadowed torment konkuri-to moonlite darkdeath
-	fanatic_castle_trap orion nmp10 katrez_d thor frostbyte ogrosupply kmap5
-	thetowers guacamole tejen hades paradigm mechanic wdcd]
-
-global small_ffa_maps [complex douze ot academy metl2 metl3 tartech fanatic_quake
-	turbine oddworld aard3c kffa neondevastation hog2 memento neonpanic sdm1
-	shinmei1 island metl4 frozen orion nmp10 torment]
-
-global big_ffa_maps [refuge kalking1 dock lostinspace DM_BS1 shindou wake5
-	fragplaza pgdm frostbyte stemple powerplant killfactory corruption
-	deathtek aqueducts orbe arabic ogrosupply curvedm ruby shadowed
-	konkuri-to moonlite darkdeath fanatic_castle_trap tejen katrez_d kmap5
-	thor thetowers hades paradigm mechanic wdcd nmp8 phosgene oasis ksauer1
-	roughinery guacamole]
-
-global instagib_maps [complex douze ot academy metl2 metl3 nmp8 tartech kalking1
-	dock turbine fanatic_quake oddworld wake5 aard3c curvedm kffa
-	neondevastation hog2 neonpanic sdm1]
-
-global small_instagib_maps [complex douze ot academy metl2 metl3 nmp8 tartech
-	dock turbine oddworld aard3c kffa neondevastation hog2 memento neonpanic
-	sdm1 shinmei1 island metl4 frozen]
-
-global big_instagib_maps [refuge kalking1 fanatic_quake lostinspace DM_BS1
-	shindou stemple powerplant killfactory corruption deathtek aqueducts
-	orbe]
-
-global capture_maps [urban_c nevil_c fb_capture nmp9 c_valley lostinspace fc3
-	face-capture nmp4 nmp8 hallo monastery ph-capture hades fc4 relic
-	frostbyte venice river_c paradigm corruption asteroids ogrosupply reissen
-	akroseum duomo capture_night c_egypt tejen dust2 campo killcore3
-	damnation arabic serenity cwcastle berlin_wall valhalla redemption l_ctf
-	sacrifice core_transfer hog2 recovery]
-
-global small_capture_maps [nevil_c fb_capture nmp9 fc3 frostbyte corruption
-	asteroids reissen tejen capture_night dust2 campo damnation cwcastle
-	face-capture l_ctf recovery hog2]
-
-global ctf_maps [hallo reissen face-capture flagstone shipwreck urban_c dust2
-	berlin_wall akroseum valhalla damnation mach2 redemption tejen europium
-	capture_night l_ctf forge campo wdcd sacrifice core_transfer recovery
-	frostbyte ogrosupply killcore3 duomo fc4 konkuri-to]
-
-global small_ctf_maps [reissen face-capture damnation tejen capture_night l_ctf
-	forge campo wdcd recovery]
-
-global "teamplay_maps" &ffa_maps
-global small_teamplay_maps &small_ffa_maps
-
-global "efficiency_maps" &ffa_maps
-global small_efficiency_maps &small_ffa_maps
-global big_efficiency_maps &big_ffa_maps
-
-global "efficiency team_maps" &ffa_maps
-global "small_efficiency team_maps" &small_ffa_maps
-
-global "tactics_maps" &ffa_maps
-global small_tactics_maps &small_ffa_maps
-global big_tactics_maps &big_ffa_maps
-
-global "tactics team_maps" &ffa_maps
-global "small_tactics team_maps" &small_ffa_maps
-
-global "instagib team_maps" &instagib_maps
-global "small_instagib team_maps" &small_instagib_maps
-
-global "regen capture_maps" &capture_maps
-global "small_regen capture_maps" &small_capture_maps
-
-global "insta ctf_maps" &ctf_maps
-global "small_insta ctf_maps" &small_ctf_maps
-
-global "protect_maps" &ctf_maps
-global small_protect_maps &small_ctf_maps
-
-global "insta protect_maps" &ctf_maps
-global "small_insta protect_maps" &small_ctf_maps
-
-global "coop edit_maps" &ffa_maps
-
-global def_ctf_maps [hallo reissen flagstone face-capture shipwreck dust2
-    urban_c berlin_wall akroseum valhalla damnation mach2 redemption tejen
-    europium capture_night l_ctf forge campo wdcd sacrifice core_transfer
-    recovery]
-
-global def_capture_maps [urban_c nevil_c fb_capture nmp9 c_valley lostinspace
-    fc3 face-capture nmp4 nmp8 hallo monastery ph-capture hades fc4 relic
-    frostbyte venice paradigm corruption asteriods ogrosupply reissen akroseum
-    duomo capture_night c_egypt tejen dust2 campo killcore3 damnation arabic
-    cwcastle river_c serenity]
-
-global def_ffa_maps [complex douze ot academy metl2 metl3 nmp8 refuge tartech
-    kalking1 dock turbine fanatic_quake oddworld wake5 aard3c curvedm fragplaza
-    pgdm kffa neondevastation hog2 memento neonpanic lostinspace DM_BS1 shindou
-    sdm1 shinmei1 stemple powerplant phosgene oasis island metl4 ruby frozen
-    ksauer1 killfactory corruption deathtek aqueducts orbe roughinery shadowed
-    torment konkuri-to moonlite darkdeath fanatic_castle_trap orion nmp10 katrez_d
-    thor frostbyte ogrosupply kmap5 thetowers guacamole tejen hades paradigm mechanic
-    wdcd]
-
-global "def_teamplay_maps" &def_ffa_maps
-global "def_efficiency_maps" &def_ffa_maps
-global "def_efficiency team_maps" &def_ffa_maps
-global "def_tactics_maps" &def_ffa_maps
-global "def_tactics team_maps" &def_ffa_maps
-global "def_instagib_maps" &def_ffa_maps
-global "def_instagib team_maps" &def_ffa_maps
-global "def_regen capture_maps" &def_capture_maps
-global "def_insta ctf_maps" &def_ctf_maps
-global "def_protect_maps" &def_ctf_maps
-global "def_insta protect_maps" &def_ctf_maps
-
-global def_game_modes [ffa teamplay instagib "instagib team" efficiency
-    "efficiency team" tactics "tactics team" capture "regen capture" ctf
-    "insta ctf" protect "insta protect"]
 
 flood_protect_text 1000
 flood_protect_sayteam 1000
