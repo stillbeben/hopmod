605b2177a5d53b15a84b4a4eab179ab3969e1f6e
graham.daws <graham.daws>
2010-05-31 19:10:06 +0000

    Renamed printserverstatus to write_server_status and moved the code to utils/apps.lua.
    git-svn-id: http://hopmod.googlecode.com/svn/trunk@1776 d7818858-e1b4-11dd-a738-5fe2e3cfbcc5

--- script/base/utils.lua
+++ script/base/utils.lua
@@ -1,63 +1,9 @@
-dofile("./script/base/utils/string.lua")
-dofile("./script/base/utils/table.lua")
-dofile("./script/base/utils/gamemode.lua")
-
 -- Copied from http://lua-users.org/wiki/SimpleRound
 function round(num, idp)
   local mult = 10^(idp or 0)
   return math.floor(num * mult + 0.5) / mult
 end
 math.round = round
-
-function server.printserverstatus(filename, filemode)
-
-	if not filemode then
-		filemode = "a+"
-	end
-
-	local out = io.open(filename, filemode)
-
-	local status_rows = "PLAYERS MAP MODE MASTER HOST PORT DESCRIPTION\n"
-	local host = server.serverip
-	if #host == 0 then host="<ANY>" end
-	local mm = server.mastermode
-	local desc = string.gsub(server.servername, " ", "_")
-	if #desc == 0 then desc = "<NONE>" end
-
-	local mapname = server.map
-	if #mapname == 0 then mapname = "<NONE>" end
-
-	status_rows = status_rows .. string.format("%i/%i %s %s %i %s %i %s", server.playercount, server.maxclients, mapname, server.gamemode, mm, host, server.serverport, desc)
-
-	out:write(tabulate(status_rows))
-	out:write("\n")
-
-	if server.playercount > 0 then
-
-		local player_rows = "CN LAG PING IP CO NAME TIME STATE PRIV\n"
-
-		for p in server.gclients() do
-
-			local country = geoip.ip_to_country_code(p:ip())
-			if #country == 0 then country = "?" end
-
-			local priv = p:priv()
-			if server.master == cn then priv = "*" .. priv end
-
-			local player_row = string.format("%i %i %i %s %s %s %s %s %s",
-				p.cn, p:lag(), p:ping(), p:ip(), country, p:name(), format_duration(p:connection_time()), p:status(), priv)
-
-			player_rows = player_rows .. player_row .. "\n"
-
-		end
-
-		out:write("\n")
-		out:write(tabulate(player_rows))
-	end
-
-	out:flush()
-	out:close()
-end
 
 function pack(...)
     return arg
@@ -100,4 +46,9 @@ end
 function server.hashpassword(cn, pass)
 	return crypto.tigersum(string.format("%i %i %s", cn, server.player_sessionid(cn), pass))
 end
+
+dofile("./script/base/utils/string.lua")
+dofile("./script/base/utils/table.lua")
+dofile("./script/base/utils/gamemode.lua")
+dofile("./script/base/utils/apps.lua")
 
