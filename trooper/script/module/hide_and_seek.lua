975fba06aea68c9856d604c10f772c91a0f6f009
pureascii <pureascii>
2010-07-19 19:18:23 +0000

    fixed waitlist
    git-svn-id: http://hopmod.googlecode.com/svn/branches/hide-and-seek@1836 d7818858-e1b4-11dd-a738-5fe2e3cfbcc5

--- trooper/script/module/hide_and_seek.lua
+++ trooper/script/module/hide_and_seek.lua
@@ -284,10 +284,12 @@ server.event_handler("mapchange", function()
 	
 	for k, v in pairs(player_waitlist) do 
 		if player_waitlist[k] ~= nil then -- to be sure
-			server.unspec(k)
-			server.sleep(500, function() 
-				server.msg(blue() .. "Unspecced: " .. red() .. server.player_name(k))
-			end)
+			if server.is_valid_cn(k) then
+				server.unspec(k)
+				server.sleep(500, function() 
+					server.msg(blue() .. "Unspecced: " .. red() .. server.player_name(k))
+				end)
+			end
 			player_waitlist[k] = nil
 		end
 	end
