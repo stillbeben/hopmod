2501382585ba829775eb505d20254df54119a5f5
pureascii <pureascii>
2010-07-19 18:56:55 +0000

    made functions local, fixed mapvote (only master/admin allowed to vote, no votes possible while timers running), added info message
    git-svn-id: http://hopmod.googlecode.com/svn/branches/hide-and-seek@1826 d7818858-e1b4-11dd-a738-5fe2e3cfbcc5

--- trooper/script/module/hide_and_seek.lua
+++ trooper/script/module/hide_and_seek.lua
@@ -1,42 +1,37 @@
--- (c) 2010 by Thomas
+-- (c) 2010 by |DM|Thomas & |noVI:pure_ascii
 
 hide_and_seek = false
-hah_active_player = -1
-hah_seek_team = ""
-hah_next_player = -1
-hah_actor_frags = 0
-MAP_LOADED_ALL = false
-PLAYER_ACTIVE = { }
-hah_active_player_can_spawn = false
-GAME_ACTIVE = false
-PLAYER_SWITCHED = false
-hah_active_player_spawn_tries = 0
-caught_players = { }
-server_change_team_request = { } 
+local hah_active_player = -1
+local hah_seek_team = ""
+local hah_next_player = -1
+local hah_actor_frags = 0
+local MAP_LOADED_ALL = false
+local PLAYER_ACTIVE = { }
+local hah_active_player_can_spawn = false
+local GAME_ACTIVE = false
+local caught_players = { }
+local server_change_team_request = { } 
+local can_vote = true
+local has_time = 12 -- 12 minutes
 
-function reset_on_mapchange()
+local function reset_on_mapchange()
     hah_actor_frags = 0
     MAP_LOADED_ALL = false
     PLAYER_ACTIVE = { }
     hah_active_player_can_spawn = false
     GAME_ACTIVE = false
     PLAYER_SWITCHED = false
-    hah_active_player_spawn_tries = 0
     caught_players = { }
     server_change_team_request = { } 
+    can_vote = false
+    server.changetime(1000*60*has_time) 
 end
 
 local function setteam(cn)
-    local jointeam = ""
+    local jointeam = "hide"
     
     if caught_players[cn] ~= nil or hah_active_player == cn then
         return hah_seek_team
-    end
-    
-    if hah_seek_team == "good" then
-        jointeam = "evil"
-    else
-        jointeam = "good"
     end
     
     if server.player_team(cn) ~= jointeam then    
@@ -47,7 +42,7 @@ local function setteam(cn)
     return jointeam
 end
 
-function server.relocate_players()
+local function relocate_players()
     if server.is_valid_cn(hah_active_player) then
         hah_seek_team = server.player_team(hah_active_player)
     end
@@ -56,26 +51,34 @@ function server.relocate_players()
     end 
 end
 
-function relocate_vars()
-        if server.is_valid_cn(hah_next_player) == 0 then
-            server.msg(red() .. "ATTENTION: Given Next Seek Player CN doesnt exist!")
-            
-            while server.is_valid_cn(hah_next_player) == 0 do
-                for i, cn_ in ipairs(server.players()) do
-                    if math.random(0, 1) == 1 then
-                        hah_next_player = cn_
-                    end
-                end           
-            end
-            
-          
-            server.msg(green() .. "Set random Player as Next Seek Player: " .. server.player_name(hah_next_player))
-        end
+local function relocate_vars(set_seek)
+    if server.is_valid_cn(hah_next_player) == 0 then
+           
+		while server.is_valid_cn(hah_next_player) == 0 do
+			for i, cn_ in ipairs(server.players()) do
+				if math.random(0, 1) == 1 then
+					hah_next_player = cn_
+				end
+			end           
+		end
+				
+			  
+		server.msg(green() .. "Set random Player as Next Seek Player: " .. server.player_name(hah_next_player))
+		
+    end
+	
+	if seet_seek ~= nil then
+		if set_seek == true then
+			return 
+		end
+	end
     
     hah_active_player = hah_next_player 
         
     server.no_spawn(hah_active_player, 1)
     server.player_slay(hah_active_player)
+    server_change_team_request[hah_active_player] = true 
+    server.changeteam(hah_active_player, "seek")
     
     for i, cn_ in ipairs(server.players()) do
         if server.player_status(cn_) == "dead" and cn_ ~= hah_active_player then
@@ -85,8 +88,25 @@ function relocate_vars()
         end
     end   
     
-    server.relocate_players()
+    relocate_players()
 end
+
+server.event_handler("spectator", function(cn, val)
+	if hide_and_seek == false then return end
+	if val == 0 then
+		setteam(cn)
+	end
+end)
+
+server.event_handler("connect", function(cn)
+	if hide_and_seek == false then return end
+	local str = "\f3PLEASE READ: "..orange().."This server is currently running the Hide and Seek mode, make sure you have the current map and understand how the mode works, before you are asking to be unspecced! thank you!"
+
+	server.sleep(3000, function() server.player_msg(cn, str) end)
+	server.sleep(5000, function() server.player_msg(cn, str) end)
+	server.sleep(7000, function() server.player_msg(cn, str) end)
+
+end)
 
 server.event_handler("damage", function(client, actor)
     if hide_and_seek == false then return end
@@ -108,7 +128,7 @@ server.event_handler("frag", function(client, actor)
     if hah_active_player == actor and client ~= actor then
         hah_actor_frags = hah_actor_frags + 1
         if hah_actor_frags == 1 then
-            server.msg(blue() .. "Next seek player set: " .. server.player_name(client))
+            server.msg(red() .. string.upper("Next seek player set: \f1" .. server.player_name(client)))
             hah_next_player = client
         end
     end
@@ -116,18 +136,48 @@ server.event_handler("frag", function(client, actor)
         if hah_seek_team == server.player_team(actor) then
             local count = 0
             for i, cn_ in ipairs(server.clients()) do
-                if server.player_status ~= "spectator" and hah_seek_team ~= server.player_team(cn_) then
+                if server.player_status(cn_) ~= "spectator" and hah_seek_team ~= server.player_team(cn_) then
                     count = count + 1  
                 end
             end
             count = count - 1
-            server.msg(green() .. server.player_name(actor) .. " got " .. server.player_name(client) .. " - " .. count .. " Players left!")
-            server_change_team_request[cn] = true 
+	    local str = "Players"
+	    if count == 1 then
+		str = "Player"
+	    end
+            server.msg(orange() .. server.player_name(actor) .. " got " .. server.player_name(client) .. " - " .. count .. " " .. str .. " left!")
+            server_change_team_request[client] = true 
             server.changeteam(client, hah_seek_team)
             caught_players[client] = true
             if count == 0 then
                 server.changetime(0)
             end
+        end
+    end
+end)
+
+server.event_handler("suicide", function(cn)
+    if hide_and_seek == false then return end
+    if server_change_team_request[cn] == true then return end
+    if hah_seek_team ~= server.player_team(cn) then
+        hah_actor_frags = hah_actor_frags + 1
+        if hah_actor_frags == 1 then
+            server.msg(blue() .. "Next seek player set: " .. server.player_name(cn))
+            hah_next_player = cn
+        end
+        local count = 0
+        for i, cn_ in ipairs(server.clients()) do
+            if server.player_status(cn_) ~= "spectator" and hah_seek_team ~= server.player_team(cn_) then
+                count = count + 1  
+            end
+        end
+        count = count - 1
+        server.msg(green() .. server.player_name(cn) .. " suicided and became a seeker - " .. count .. " Players left!")
+        server_change_team_request[cn] = true 
+        server.changeteam(cn, hah_seek_team)
+        caught_players[cn] = true
+        if count == 0 then
+            server.changetime(0)
         end
     end
 end)
@@ -146,15 +196,20 @@ end)
 
 ALREADY_DONE = false
 
-server.event_handler("mapchange", function(cn)
+server.event_handler("mapchange", function()
 	if hide_and_seek == false then return end
-    server.msg(green() .. "waiting for clients to load the map...")
+    	server.msg(green() .. "waiting for clients to load the map...")
+	for i, cn in ipairs(server.clients()) do
+		server.player_slay(cn)
+		server.no_spawn(cn, 1)
+	end
+	reset_on_mapchange() 
 end)
 
 server.event_handler("spectator", function(cn, join)
     if hide_and_seek == false then return end
     if join == 0 then
-        server.relocate_players()
+        relocate_players()
     end
 end)
 
@@ -170,6 +225,26 @@ server.event_handler("reteam", function(cn, old, new)
     server.player_msg(cn, red() .. "You can't switch the team!")
 end)
 
+server.event_handler("mapvote", function(cn, map, mode)
+	if hide_and_seek == false then return end
+	if server.player_priv(cn) ~= "master" and server.player_priv(cn) ~= "admin" then
+		server.player_msg(cn, red() .. "Only Master/Admin can set a Map!")
+		return -1
+	else	
+		if can_vote == false then
+			server.player_msg(cn, red() .. "Please wait until the seek player spawned for a new mapvote!")
+			return -1
+		else
+			if mode ~= "teamplay" then
+				server.player_msg(cn, red() .. "Hide and Seek can only be played in Teamplay-Mode!")
+				return -1
+			end
+		end
+	end
+end)
+
+
+
 server.event_handler("maploaded", function(cn)
     if hide_and_seek == false then return end
     if GAME_ACTIVE == true then
@@ -179,7 +254,7 @@ server.event_handler("maploaded", function(cn)
 	PLAYER_ACTIVE[cn] = true
 
 	local canstart = true
-	for _, cn_ in ipairs(server.players()) do
+	for _, cn_ in ipairs(server.clients()) do
 		if PLAYER_ACTIVE[cn_] == nil then
 			canstart = false 
 		end
@@ -188,50 +263,79 @@ server.event_handler("maploaded", function(cn)
     if canstart then
         GAME_ACTIVE = true
         
-        reset_on_mapchange() 
         relocate_vars()
         
-        server.msg(red() .. "Maploading finished, go and hide, the seek player will spawn in 10 seconds!")
+        server.msg(red() .. "Maploading finished!")
+
+		for i, cn_ in ipairs(server.clients()) do
+			if server.player_status(cn_) ~= "spectator" and cn_ ~= hah_active_player then
+				server.no_spawn(cn_, 0)
+				server.spawn_player(cn_)
+			end
+		end		
+
+		server.sleep(0, function()
+		
+			server.msg(green() .. "Go and hide, the seek player will spawn in 10 seconds!")
+			
+			
+			if server.player_status(hah_active_player) == "spectator" then
+				 relocate_vars(true)
+			end
         
-        for i, cn_ in ipairs(server.clients()) do
-            if server.player_status(cn_) ~= "spectator" and cn_ ~= hah_active_player then
-                server.spawn_player(cn_)
-            end
-        end
-        
-        server.sleep(5000, function()
-            server.msg(blue() .. "5 seconds, run!") 
-        end)
-        server.sleep(7000, function()
-            server.msg(red() .. "3 seconds...") 
-        end)
-        server.sleep(8000, function()
-            server.msg(red() .. "2 seconds...") 
-        end)
-        server.sleep(9000, function()
-            server.msg(red() .. "1 second....") 
-            stop_kill_event = true
-        end)
-        server.sleep(10000, function()
-            server.no_spawn(hah_active_player, 0)
-            server.spawn_player(hah_active_player)
-            server.msg(yellow() .. "Seek Player spawned!")
-        end)
+			for i, cn_ in ipairs(server.clients()) do
+				if server.player_status(cn_) ~= "spectator" and cn_ ~= hah_active_player then
+					server.spawn_player(cn_)
+				end
+			end
+			
+			server.sleep(5000, function()
+				server.msg(blue() .. "5 seconds, run!") 
+			end)
+			server.sleep(7000, function()
+				server.msg(red() .. "3 seconds...") 
+			end)
+			server.sleep(8000, function()
+				server.msg(red() .. "2 seconds...") 
+			end)
+			server.sleep(9000, function()
+				server.msg(red() .. "1 second....") 
+				stop_kill_event = true
+			end)
+			server.sleep(10000, function()
+				server.no_spawn(hah_active_player, 0)
+				server.spawn_player(hah_active_player)
+				server.msg(yellow() .. "Seek Player spawned!")
+				can_vote = true
+				server.changetime(1000*60*has_time)
+			end)
+		
+		end)
     end
 end)
 
+server.event_handler("disconnect", function(cn, reason)
+    if server.playercount == 0 and hide_and_seek == true then
+        server.mastermode = 0
+        hide_and_seek = false
+    end
+end)
 
-function server.playercmd_hah(cn, enable)
+function server.playercmd_has(cn, enable)
     enable = tonumber(enable)
     if enable == 1 then
+	server.broadcast_mapmodified = false
         hide_and_seek = true
+	server.HIDE_AND_SEEK = 1
         server.mastermode = 2
         server.msg("mastermode is now locked (2)")
         server.msg(green() .. "Hide and Seek Mode enabled!")
+	can_vote = true
     else
+	server.broadcast_mapmodified = true
+	server.HIDE_AND_SEEK = 0
         server.msg(blue() .. "Hide and Seek Mode disabled!")
+        server.mastermode = 0
         hide_and_seek = false
-        return
     end
 end
-
