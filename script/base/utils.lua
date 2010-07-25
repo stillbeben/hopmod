95c4a875c3683312dc695bc7576910e4fb58bc7f
graham.daws <graham.daws>
2010-07-25 18:36:34 +0000

    Fixed strange behaviour in catch_error: give start and end index to unpack so that it won't stop after nil elements.
    git-svn-id: http://hopmod.googlecode.com/svn/trunk@1874 d7818858-e1b4-11dd-a738-5fe2e3cfbcc5

--- script/base/utils.lua
+++ script/base/utils.lua
@@ -1,9 +1,9 @@
 -- Copied from http://lua-users.org/wiki/SimpleRound
-function round(num, idp)
+function math.round(num, idp)
   local mult = 10^(idp or 0)
   return math.floor(num * mult + 0.5) / mult
 end
-math.round = round
+round = math.round
 
 function pack(...)
     return arg
@@ -13,28 +13,21 @@ function identity(...)
     return unpack(arg)
 end
 
-function catch_error(fun, ...)
-
-    local returnvals = pack(pcall(fun, unpack(arg)))
-    
-    if returnvals[1] == false then
-        server.log_error(returnvals[2])
-        return
-    end
-    
-    table.remove(returnvals, 1)
-    return unpack(returnvals)
-end
-
 function return_catch_error(fun, ...)
 
     local returnvals = pack(pcall(fun, unpack(arg)))
-    
-    if returnvals[1] == false then
+        
+    if returnvals[1] == false and returnvals[2] then
         server.log_error(returnvals[2])
     end
     
-    return unpack(returnvals)
+    return unpack(returnvals, 1, table.maxn(returnvals))
+end
+
+function catch_error(fun, ...)
+    local returnvals = pack(return_catch_error(fun, unpack(arg)))
+    table.remove(returnvals, 1)
+    return unpack(returnvals, 1, table.maxn(returnvals))
 end
 
 function server.eval_lua(str)
