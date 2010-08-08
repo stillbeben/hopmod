7d7be2b140be2673537b4044abd051045f41ea55
zombie@c-c-z.org <zombie@c-c-z.org>
2010-08-08 14:35:50 +0000

    updated 'inactivity' module
    git-svn-id: http://hopmod.googlecode.com/svn/trunk@1901 d7818858-e1b4-11dd-a738-5fe2e3cfbcc5

--- script/module/limit/inactivity.lua
+++ script/module/limit/inactivity.lua
@@ -3,54 +3,79 @@
 ]]
 
 local interval_time = server.spec_inactives_check_time
-
 if interval_time <= 0 then
-	interval_time = 60 * 1000
+    interval_time = 60000
 end
-
 local inactive_time = round((server.spec_inactives_time / 1000), 0)
-local is_unload = false
+local death_only = server.spec_inactives_death_only == 1
 
-local function set_deathtime(cn)
-	server.player_vars(cn).spec_inactives_deathtime = server.player_connection_time(cn)
-end
+local is_unload
 
-server.event_handler("frag", set_deathtime) -- first argument of frag event is target cn
-server.event_handler("suicide", set_deathtime)
 
 server.interval(interval_time, function()
 
-	if is_unload == true then
-		return -1
-	end
-
-	if server.mastermode == 0 then
-		for p in server.gplayers() do
-			local deathtime = p:vars().spec_inactives_deathtime
-
-			if deathtime then
-				if p:status_code() == server.DEAD then
-					if p:connection_time() - deathtime >= inactive_time then
-						p:spec()
-						p:msg("You joined spectators, because you seem to be inactive - type " .. yellow("/spectator 0") .. " to rejoin the game.")
-						p:vars().spec_inactives_deathtime = nil
-					end
-				else
-					p:vars().spec_inactives_deathtime = nil
-				end
+    if is_unload
+    then
+	return -1
+    end
+    
+    if (server.mastermode == 0) or (server.mastermode == 1)
+    then
+	for p in server.gplayers()
+	do
+	    local spec_player
+	    
+	    local last_x, last_y, last_z = p:vars().spec_inactives_pos_x, p:vars().spec_inactives_pos_y, p:vars().spec_inactives_pos_z
+	    
+	    if not (last_x and last_y and last_z)
+	    then
+	        p:vars().spec_inactives_pos_x, p:vars().spec_inactives_pos_y, p:vars().spec_inactives_pos_z = p:pos()
+	        p:vars().spec_inactives_pos_time = p:connection_time()
+	    else
+	        local x, y, z = p:pos()
+		
+		if (last_x == x) and (last_y == y) and (last_z == z)
+		then
+		    local last_time = p:vars().spec_inactives_pos_time
+		    
+		    if p:connection_time() - last_time >= inactive_time
+		    then
+			spec_player = true
+			
+			if death_only and not (p:status_code() == server.DEAD)
+			then
+			    spec_player = nil
 			end
+			
+			p:vars().spec_inactives_pos_x, p:vars().spec_inactives_pos_y, p:vars().spec_inactives_pos_z = nil, nil, nil
+			p:vars().spec_inactives_pos_time = nil
+		    end
+		else
+		    p:vars().spec_inactives_pos_x, p:vars().spec_inactives_pos_y, p:vars().spec_inactives_pos_z = nil, nil, nil
+		    p:vars().spec_inactives_pos_time = nil
 		end
+	    end
+	    
+	    if spec_player
+	    then
+		p:spec()
+		p:msg("Server moved you to spectators, because you seem to be inactive - type " .. yellow("/spectator 0") .. " to rejoin the game.")
+	    end
 	end
-	
+    end
 end)
+
 
 local function unload()
 
-	is_unload = true
-
-	for p in server.aplayers() do
-		p:vars().spec_inactives_deathtime = nil
-	end
+    is_unload = true
+    
+    for p in server.gclients() do
+	p:vars().spec_inactives_deathtime = nil
+	p:vars().spec_inactives_pos_x, p:vars().spec_inactives_pos_y, p:vars().spec_inactives_pos_z = nil, nil, nil
+	p:vars().spec_inactives_pos_time = nil
+    end
 end
+
 
 return {unload = unload}
