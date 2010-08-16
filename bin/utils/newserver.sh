07728b874ee9084094ee4d07babd5e7c4e75442c
zombie@c-c-z.org <zombie@c-c-z.org>
2010-08-16 10:16:28 +0000

    Updated newserver script
    git-svn-id: http://hopmod.googlecode.com/svn/trunk@1930 d7818858-e1b4-11dd-a738-5fe2e3cfbcc5

--- bin/utils/newserver.sh
+++ bin/utils/newserver.sh
@@ -8,17 +8,38 @@ fi
 
 DEST=$1
 
+
+v_system=$(uname)
+f_starter=$(basename $0)
+d_pwd=$(pwd)
+d_main=
+
+if echo $v_system | grep -q "BSD"
+then
+    d_main=$(dirname $(dirname $(cd $(dirname $0) ; pwd)))
+    cd $d_pwd
+else
+    d_main=$(dirname $(dirname $(dirname $(readlink -f $0))))
+fi
+
+
 mkdir -p $DEST
 
-ln -s $PWD/bin $DEST/bin
-ln -s $PWD/lib $DEST/lib
-ln -s $PWD/script $DEST/script
-ln -s $PWD/share $DEST/share
+mkdir $DEST/bin
+for aa in utils env.sh sauer_server
+do
+    ln -s $d_main/bin/$aa $DEST/bin/$aa
+done
+cp $d_main/bin/server $DEST/bin/
+
+ln -s $d_main/lib $DEST/lib
+ln -s $d_main/script $DEST/script
+ln -s $d_main/share $DEST/share
 
 mkdir $DEST/conf
 mkdir $DEST/log
 mkdir $DEST/log/game
 mkdir $DEST/log/demo
 
-cp conf/server.conf $DEST/conf/server.conf
-cp conf/maps.conf $DEST/conf/maps.conf
+cp $d_main/conf/server.conf $DEST/conf/server.conf
+cp $d_main/conf/maps.conf $DEST/conf/maps.conf
