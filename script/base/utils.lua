27e502d4e34d7e017c610969208488b95be080e5
graham.daws <graham.daws>
2010-08-29 02:06:02 +0000

    Added bind and _if global lua functions
    git-svn-id: http://hopmod.googlecode.com/svn/trunk@1954 d7818858-e1b4-11dd-a738-5fe2e3cfbcc5

--- script/base/utils.lua
+++ script/base/utils.lua
@@ -40,6 +40,57 @@ function server.hashpassword(cn, password)
 	return crypto.tigersum(string.format("%i %i %s", cn, server.player_sessionid(cn), password))
 end
 
+bind_placeholder = {
+
+    is_bind_placeholder = true,
+    
+    create = function(index)
+        local obj = {index=index}
+        setmetatable(obj, bind_placeholder)
+        return obj
+    end,
+    
+    detected = function(obj)
+        local mt = getmetatable(obj)
+        return mt and mt.is_bind_placeholder
+    end
+}
+
+_1 = bind_placeholder.create(1)
+_2 = bind_placeholder.create(2)
+_3 = bind_placeholder.create(2)
+_4 = bind_placeholder.create(4)
+_5 = bind_placeholder.create(5)
+_6 = bind_placeholder.create(6)
+
+function bind(func, ...)
+
+    local bind_args = arg
+    
+    return function(...)
+    
+        local call_args = {}
+        
+        for index, value in pairs(bind_args) do
+            if bind_placeholder.detected(value) then
+                call_args[index] = arg[value.index]
+            else
+                call_args[index] = value
+            end
+        end
+        
+        return func(unpack(call_args, 1, table.maxn(bind_args)))
+    end
+end
+
+function _if(expr, true_value, false_value)
+    if expr then
+        return true_value
+    else
+        return false_value
+    end
+end
+
 dofile("./script/base/utils/apps.lua")
 dofile("./script/base/utils/file.lua")
 dofile("./script/base/utils/gamemode.lua")
