ec3dfd8ce516468171c1dae3e460f1d541fa8337
t.poechtrager@gmail.com <t.poechtrager@gmail.com>
2010-09-18 17:54:04 +0000

    added debug argument to the compile script
    git-svn-id: http://hopmod.googlecode.com/svn/trunk@1982 d7818858-e1b4-11dd-a738-5fe2e3cfbcc5

--- compile.sh
+++ compile.sh
@@ -2,18 +2,26 @@
 THREADS=`cat /proc/cpuinfo | grep processor | wc -l`
 ARG_LENGTH=$# 
 STRCOMPILE="Compiling"
+COMPILEDIR="release_build"
+COMPILEFLAGS=""
+BUILDTYPE="release"
+if [ "$ARG_LENGTH" > 1 -a "$2" = "debug" ]; then
+  COMPILEDIR="debug_build"
+  COMPILEFLAGS="-D CMAKE_BUILD_TYPE=DEBUG"
+  BUILDTYPE="debug"
+fi
 if [ "$ARG_LENGTH" > 0 -a "$1" = 1 ]; then
   STRCOMPILE="Recompiling"
-  rm -rf release-build
+  rm -rf $COMPILEDIR
 fi
-mkdir release-build
-cd release-build 
-cmake .. >> /dev/null
+mkdir $COMPILEDIR
+cd $COMPILEDIR
+cmake $COMPILEFLAGS .. >> /dev/null
 STRTHREADS="threads"
 if [ $THREADS = 1 ]; then
   STRTHREADS="thread"
 fi
-echo "$STRCOMPILE Hopmod using $THREADS $STRTHREADS\n"
+echo "$STRCOMPILE Hopmod using $THREADS $STRTHREADS ($BUILDTYPE build)\n"
 TS_START=`date +%s`
 make -j$THREADS 
 make install >> /dev/null
