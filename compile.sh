3f6113b2d1195b9e4a32afd89f43c930cf42f08b
t.poechtrager@gmail.com <t.poechtrager@gmail.com>
2010-09-18 17:40:49 +0000

    updated compile script
    git-svn-id: http://hopmod.googlecode.com/svn/trunk@1981 d7818858-e1b4-11dd-a738-5fe2e3cfbcc5

--- compile.sh
+++ compile.sh
@@ -1,5 +1,22 @@
+#!/bin/sh
+THREADS=`cat /proc/cpuinfo | grep processor | wc -l`
+ARG_LENGTH=$# 
+STRCOMPILE="Compiling"
+if [ "$ARG_LENGTH" > 0 -a "$1" = 1 ]; then
+  STRCOMPILE="Recompiling"
+  rm -rf release-build
+fi
 mkdir release-build
-cd release-build
-cmake ..
-make
-make install
+cd release-build 
+cmake .. >> /dev/null
+STRTHREADS="threads"
+if [ $THREADS = 1 ]; then
+  STRTHREADS="thread"
+fi
+echo "$STRCOMPILE Hopmod using $THREADS $STRTHREADS\n"
+TS_START=`date +%s`
+make -j$THREADS 
+make install >> /dev/null
+TS_END=`date +%s`
+TS_DIFF=`echo $TS_END $TS_START | awk '{print $1 - $2}'`
+echo "\nTook $TS_DIFF Seconds"
