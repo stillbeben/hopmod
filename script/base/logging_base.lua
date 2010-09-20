ea9aabafdafca94b407e4f085eb6365a9e89f17b
zombie@c-c-z.org <zombie@c-c-z.org>
2010-09-20 22:31:06 +0000

    prevent init of the functions log_status and log_error, when server is an authserver
    git-svn-id: http://hopmod.googlecode.com/svn/trunk@1999 d7818858-e1b4-11dd-a738-5fe2e3cfbcc5

--- script/base/logging_base.lua
+++ script/base/logging_base.lua
@@ -1,6 +1,8 @@
+local is_authserver = (server.is_authserver or 0) == 1
+
 local filename = "server"
 
-if (server.is_authserver or 0) == 1 then
+if is_authserver then
     filename = "authserver"
 end
 
@@ -14,17 +16,18 @@ function server.log(msg)
     logfile:flush()
 end
 
-function server.log_status(msg)
-    print(msg)
-end
-
-function server.log_error(msg)
-    assert(msg ~= nil)
-    io.stderr:write(os.date("[%a %d %b %X] ",os.time()))
-    io.stderr:write(msg)
-    io.stderr:write("\n")
-    io.stderr:flush()
-end
-
 server.event_handler("shutdown", bind(logfile.close, logfile))
 
+if not server.is_authserver then
+    function server.log_status(msg)
+	print(msg)
+    end
+    
+    function server.log_error(msg)
+	assert(msg ~= nil)
+	io.stderr:write(os.date("[%a %d %b %X] ",os.time()))
+	io.stderr:write(msg)
+	io.stderr:write("\n")
+	io.stderr:flush()
+    end
+end
