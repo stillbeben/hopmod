4035f056749534a28b3d7b223a85446ec0cd009c
zombie@c-c-z.org <zombie@c-c-z.org>
2010-09-20 21:34:49 +0000

    updated name(protection) module
    git-svn-id: http://hopmod.googlecode.com/svn/trunk@1992 d7818858-e1b4-11dd-a738-5fe2e3cfbcc5

--- script/module/auth/name.lua
+++ script/module/auth/name.lua
@@ -1,91 +1,139 @@
-local domain = server.name_reservation_domain
-local reserved_name_expire = server.reserved_name_expire
+local domain		= server.name_reservation_domain
 
-local failure_message = {}
-failure_message[auth.request_status.REQUEST_FAILED] = "You are using a reserved name."
-failure_message[auth.request_status.CHALLENGE_FAILED] = "The server failed to authenticate you for the use of the reserved name."
-failure_message[auth.request_status.RESPONSE_FAILED] = failure_message[auth.request_status.CHALLENGE_FAILED]
-failure_message[auth.request_status.TIMEOUT] = failure_message[auth.request_status.CHALLENGE_FAILED]
-failure_message["WRONG_KEY"] = "You authenticated with a key for another user."
-
-local function player_verified(cn)
-
-	local pvars = server.player_vars(cn)
-
-	return pvars.name_verified and pvars.reserved_name and pvars.nameprotect_wanted_authname and pvars.reserved_name_expire and string.lower(pvars.reserved_name) == string.lower(pvars.nameprotect_wanted_authname) and tonumber(server.uptime) < pvars.reserved_name_expire
-
+if not auth.directory.get_domain(domain)
+then
+    server.log_error("Name protection auth doesn't work, but is required: unknown domain " .. tostring(domain))
+    return
 end
 
+local MIN_CACHE_TIME	= 3 * 60 * 60 * 1000
+
+local failure_message = {
+    [auth.request_status.REQUEST_FAILED]	= "You are using a reserved name.",
+    [auth.request_status.CHALLENGE_FAILED]	= "The server failed to authenticate you for the use of the reserved name.",
+    ["WRONG_KEY"]				= "You authenticated with a key for another user."
+}
+failure_message[auth.request_status.RESPONSE_FAILED]	= failure_message[auth.request_status.CHALLENGE_FAILED]
+failure_message[auth.request_status.TIMEOUT]		= failure_message[auth.request_status.CHALLENGE_FAILED]
+
+local name_cache = {}
+
+
+server.name_protection_loaded = true
+
+
+local function rename(cn)
+
+    server.player_rename(cn, "unnamed", true)
+    
+    server.player_msg(cn, "You have used a reserved name of another player. Server renamed you to 'unnamed'.")
+    server.player_msg(cn, " ")
+end
+
+local function request_key(cn)
+
+    local sid = server.player_sessionid(cn)
+    
+    auth.send_request(cn, domain, function(cn, user_id, domain, status)
+    
+	if not (sid == server.player_sessionid(cn))
+	then
+	    return
+	end
+	
+        local v = server.player_vars(cn)
+	
+	if status == auth.request_status.SUCCESS
+	then
+	    if v.nameprotect_wanted_authname == string.lower(user_id)
+	    then
+		v.reserved_name = user_id
+		server.log(string.format("%s(%i) authenticated as '%s' to use reserved name.", server.player_name(cn), cn, user_id))
+		return
+	    else
+		status = "WRONG_KEY"
+	    end
+	end
+	
+	if status == auth.request_status.CANCELLED
+	then
+	    return
+	end
+	
+	server.player_msg(cn, " ")
+	server.player_msg(cn, red(failure_message[status]))
+	
+	rename(cn)
+    end)
+end
 
 local function check_name(cn)
 
-	local name = server.player_name(cn)
-
-	if not player_verified(cn) then 
-		server.player_vars(cn).nameprotect_wanted_authname = name
-
-		auth.query_id(string.lower(name), domain, function(result)
-
-			if not result or result == false then
-				return
-			end
-
-			if result == true then
-				auth.send_request(cn, domain, function(cn, user_id, domain, status)
-
-					local pvars = server.player_vars(cn)
-
-					if status == auth.request_status.SUCCESS then
-						if string.lower(pvars.nameprotect_wanted_authname) == string.lower(user_id) then
-							server.log(string.format("%s(%i) authenticated as '%s' to use reserved name.", server.player_name(cn), cn, user_id))
-
-							local vars = server.player_vars(cn)
-
-							pvars.name_verified = true
-							pvars.reserved_name = user_id
-							pvars.reserved_name_expire = tonumber(server.uptime) + tonumber(reserved_name_expire)
-
-							vars.name_verified = true
-							vars.reserved_name = user_id
-							vars.reserved_name_expire = tonumber(server.uptime) + tonumber(reserved_name_expire)
-
-							return
-						else
-							status = "WRONG_KEY"
-						end
-					end
-
-					if status == auth.request_status.CANCELLED then
-						return
-					end
-
-					server.player_msg(cn, red(failure_message[status]))
-					server.player_msg(cn, "You have 10 seconds to rename.")
-
-					local session_id = server.player_sessionid(cn)
-					local old_name = server.player_name(cn)
-
-					server.sleep(10000, function()
-
-						if server.player_sessionid(cn) ~= session_id then
-							return
-						end
-
-						if server.player_name(cn) == old_name then
-							server.kick(cn, 0, "server", "use of reserved name")
-						end
-
-					end)
-
-				end)
-
-			end
-
-		end)
-
+    local v = server.player_vars(cn)
+    
+    v.nameprotect_wanted_authname = string.lower(server.player_name(cn))
+    
+    if v.reserved_name and not (string.lower(v.reserved_name) == v.nameprotect_wanted_authname)
+    then
+        if name_cache[v.nameprotect_wanted_authname]
+        then
+    	    rename(cn)
+	else
+	    auth.query_id(v.nameprotect_wanted_authname, domain, function(result)
+	    
+		if result
+		then
+		    name_cache[server.player_vars(cn).nameprotect_wanted_authname] = server.uptime
+		    
+		    rename(cn)
+		end
+	    end)
 	end
-
+	
+    elseif name_cache[v.nameprotect_wanted_authname]
+    then
+        request_key(cn)
+    else
+        auth.query_id(v.nameprotect_wanted_authname, domain, function(result)
+	
+	    if result
+	    then
+		name_cache[server.player_vars(cn).nameprotect_wanted_authname] = server.uptime
+		
+		request_key(cn)
+	    end
+	end)
+    end
 end
+
 
 server.event_handler("connect", check_name)
 
+server.event_handler("rename", check_name)
+
+server.event_handler("disconnect", function(cn)
+
+    local v = server.player_vars(cn)
+    
+    v.nameprotect_wanted_authname	= nil
+    v.reserved_name			= nil
+end)
+
+server.event_handler("maintenance", function()
+
+    local cur_time = server.uptime
+    
+    for name, time in pairs(name_cache)
+    do
+	if (cur_time - time) > MIN_CACHE_TIME
+	then
+	    name_cache[name] = nil
+	end
+    end
+end)
+
+
+return {unload = function()
+
+    server.name_protection_loaded = nil
+end}
