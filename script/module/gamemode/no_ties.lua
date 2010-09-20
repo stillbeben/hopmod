efcfa856c0c177748f70f86f87b30728a013d3a2
zombie@c-c-z.org <zombie@c-c-z.org>
2010-09-20 21:23:12 +0000

    updated no_ties module
    git-svn-id: http://hopmod.googlecode.com/svn/trunk@1989 d7818858-e1b4-11dd-a738-5fe2e3cfbcc5

--- script/module/gamemode/no_ties.lua
+++ script/module/gamemode/no_ties.lua
@@ -9,8 +9,7 @@
 
 local default_enabled = server.no_ties_enabled_by_default == 1
 
-local is_active
-local is_new_map
+local is_enabled
 
 local msg_overtime = "One minute overtime!"
 
@@ -19,98 +18,74 @@ function server.no_ties(enable)
 
     if not enable
     then
-	is_active = nil
+	is_enabled = nil
     else
-	is_active = true
+	is_enabled = true
     end
 end
 
+
+server.event_handler("timeupdate", function(mins, secs)
+
+    if (mins == 0) and (secs == 0) and is_enabled and not gamemodeinfo.edit
+    then
+	local scores = {}
+	
+	if gamemodeinfo.teams
+	then
+	    for _, team in pairs(server.teams())
+	    do
+		local score = server.team_score(team)
+		
+		if scores[score]
+		then
+		    server.changetime(60000)
+		    server.msg(msg_overtime)
+		    break
+		end
+		
+		scores[score] = team
+	    end
+	else
+    	    for p in server.gplayers()
+    	    do
+    		local score = p:score()
+    		
+    		if scores[score]
+		then
+		    server.changetime(60000)
+		    server.msg(msg_overtime)
+		    break
+		end
+		
+		scores[score] = p.cn
+	    end
+	end
+    end
+end)
+
+server.event_handler("finishedgame", function()
+
+    is_enabled = nil
+end)
+
+server.event_handler("mapchange", function()
+
+    is_enabled = default_enabled
+end)
 
 server.event_handler("connect", function()
 
     if server.playercount == 1
     then
-	if default_enabled
-	then
-    	    server.no_ties(true)
-	else
-    	    server.no_ties()
-	end
+	is_enabled = default_enabled
     end
-end)
-
-server.event_handler("timeupdate", function(mins)
-
-    if (mins == 1) and is_active and not gamemodeinfo.edit
-    then
-	is_new_map = nil
-	
-	server.sleep(58000, function()
-	
-	    if is_active and not is_new_map
-	    then
-		local is_tie
-		
-		if gamemodeinfo.teams
-		then
-		    for _, team in pairs(server.teams())
-		    do
-			local score = server.team_score(team)
-			
-			for _, t in pairs(server.teams()) do
-	    		    if not (team == t) and (score == server.team_score(t))
-	    		    then
-				is_tie = true
-				break
-			    end
-			end
-			
-			if is_tie
-			then
-			    break
-			end
-		    end
-		else
-    		    for player in server.gplayers()
-    		    do
-    			local cn = player.cn
-    			local score = player:score()
-    			
-			for p in server.gplayers()
-			do
-	    		    if not (cn == p.cn) and (score == p:score())
-	    		    then
-				is_tie = true
-				break
-			    end
-			end
-			
-			if is_tie
-			then
-			    break
-			end
-		    end
-		end
-		
-		if is_tie
-		then
-		    server.msg(msg_overtime)
-		    server.changetime(60000)
-		end
-	    end
-	end)
-    end
-end)
-
-server.event_handler("mapchange", function()
-
-    is_new_map = true
 end)
 
 
 return {unload = function()
 
-    is_active, is_new_map = nil, nil
+    is_enabled = nil
     
     server.unref("no_ties")
 end}
