ee279beeb8f79899739afdfe1b9fc14759282bd3
zombie@c-c-z.org <zombie@c-c-z.org>
2010-09-22 22:26:58 +0000

    updated all team balancers
    git-svn-id: http://hopmod.googlecode.com/svn/trunk@2008 d7818858-e1b4-11dd-a738-5fe2e3cfbcc5

--- script/module/balance/teams/by_adding_bots.lua
+++ script/module/balance/teams/by_adding_bots.lua
@@ -1,45 +1,45 @@
 --[[
     Auto Team Balancing Using Bots
+    
+    blocks teamswitchs against the balance
+	teambalance_using_moveblock
 ]]
 
-local using_bots = server.teambalance_using_bots
-local using_moveblock = server.teambalance_using_moveblock
-local bots_added = 0
+local using_moveblock = server.teambalance_using_moveblock == 1
+local bot_skill_low = server.teambalance_bot_skill_low
+local bot_skill_high = server.teambalance_bot_skill_high
 
-local function team_sizes()
-    
-    local teams = {}
-    local greatest_teamsize = 0
-    
-    local function increment(t, key)
-        local newvalue = (t[key] or 0) + 1
-        t[key] = newvalue
-        return newvalue
-    end
-    
-    for player in server.gplayers() do
-        greatest_teamsize = math.max(greatest_teamsize, increment(teams, player:team()))
-    end
-    
-    -- Discounting bot players
-    
-    return teams, greatest_teamsize
+if bot_skill_low and (bot_skill_high or 0) < bot_skill_low then
+    bot_skill_high = bot_skill_low
 end
+
+local bots_added = 0
+local has_flag = {}
+
 
 local function addbots(num)
     
     if num == 0 then return end
     
-    for x = 1, num do server.addbot(-1) end
+    if bot_skill_low then
+	for x = 1, num do server.addbot(math.random(bot_skill_low, bot_skill_high)) end
+    else
+	for x = 1, num do server.addbot(-1) end
+    end
 end
 
 local function delbots(num)
+
     local bots = server.bots()
     local num = math.min(num, #bots)
-    for x = 1, num do server.delbot(bots[x]) end
+    
+    for x = 1, num do
+	if not has_flag[127 + x] then server.delbot(bots[x]) end
+    end
 end
 
 local function remove_bots()
+
     if bots_added > 0 then
         delbots(bots_added)
         bots_added = 0
@@ -48,33 +48,31 @@ end
 
 local function check_balance()
 
-    if not gamemodeinfo.teams or server.mastermode > 1 then -- deactivate conditions
+    if not gamemodeinfo.teams or server.mastermode > 0 then -- deactivate conditions
         remove_bots()
         return
     end
     
-    local teams = team_sizes()
+    local teams = server.team_sizes()
     local size_difference = math.abs((teams.good or 0) - (teams.evil or 0))
     local unbalanced = size_difference > 0
     
     if unbalanced then
-        if using_bots then
-            
-            local change = size_difference - bots_added
+        local change = size_difference - bots_added
         
-            if change ~= 0 then
-                
-                local change_function = addbots
-                if change < 0 then change_function = delbots end
-                change_function(math.abs(change))
-                
-                bots_added = bots_added + change
-            end
+        if change ~= 0 then
+        
+    	    local change_function = addbots
+            if change < 0 then change_function = delbots end
+            change_function(math.abs(change))
+            
+            bots_added = bots_added + change
         end
     else
         remove_bots()
     end
 end
+
 
 server.event_handler("disconnect",  check_balance)
 server.event_handler("connect",     check_balance)
@@ -82,29 +80,63 @@ server.event_handler("spectator",   check_balance)
 server.event_handler("reteam",      check_balance)
 
 server.event_handler("mapchange", function()
-    bots_added = 0 -- the server disconnects all the bots at the end of the game
+
+    has_flag	= {}
+    bots_added	= 0 -- the server disconnects all the bots at the end of the game
     check_balance() -- for one player case
 end)
 
-server.event_handler("chteamrequest", function(cn, curteam, newteam)
-    local teams = team_sizes()
-    if (teams[curteam] or 0) > (teams[newteam] or 0) and using_moveblock then
-        server.player_msg(cn, red(string.format("Team change disallowed: \"%s\" team has enough players.", newteam)))
-        return -1
+if using_moveblock
+then
+    server.event_handler("chteamrequest", function(cn, curteam, newteam)
+    
+	local teams = server.team_sizes()
+	
+	if (teams[curteam] or 0) > (teams[newteam] or 0) then
+    	    server.player_msg(cn, red(string.format("Team change disallowed: \"%s\" team has enough players.", newteam)))
+    	    return -1
+	end
+    end)
+end
+
+server.event_handler("setmastermode", function(cn, current, new)
+
+    if new ~= "open" and bots_added then
+        remove_bots()
+        server.player_msg(cn, "Auto Team Balancing has been disabled. It will be re-enabled once the bots have been removed and/or the mastermode has been set to OPEN(0).")
     end
 end)
 
-server.event_handler("setmastermode", function(cn, current, new)
-    if (new ~= "open" and new ~= "veto") and bots_added then
-        remove_bots()
-        server.player_msg(cn, "Auto Team Balancing has been disabled. It will be re-enabled once the bots have been removed and/or the mastermode has been set to OPEN(0) or VETO(1).")
+server.event_handler("takeflag", function(cn)
+
+    if is_enabled() and server.is_bot(cn) then
+	has_flag[cn] = true
     end
 end)
+
+server.event_handler("scoreflag", function(cn)
+
+    if is_enabled() and server.is_bot(cn) then
+	has_flag[cn] = nil
+	check_balance()
+    end
+end)
+
+server.event_handler("dropflag", function(cn)
+
+    if is_enabled() and server.is_bot(cn) then
+	has_flag[cn] = nil
+	check_balance()
+    end
+end)
+
 
 local function unload_module()
     remove_bots()
 end
 
+
 check_balance() -- in case this module was loaded from #reload
+
 
 return {unload = unload_module}
--- script/module/balance/teams/by_moving_players.lua
+++ script/module/balance/teams/by_moving_players.lua
@@ -1,239 +1,222 @@
 --[[
-
-	player moving team balancing
-
+    player moving team balancing
+    
+    disallow other teams than good and evil [not optional]
+    blocks teamswitchs against the balance
+        teambalance_using_moveblock
+    reteams spectator leaving players, when it helps to balance
+        teambalance_using_player_moving_balancer_when_leaving_spec
+    reteams dead players, that cry balance, while they are in the fuller team
+        teambalance_using_text_addin
 ]]
 
-local using_moveblock = server.teambalance_using_moveblock
-local using_player_moving_balancer_when_leaving_spec = server.teambalance_using_player_moving_balancer_when_leaving_spec
-local using_text_addin = server.teambalance_using_text_addin
+local using_moveblock = server.teambalance_using_moveblock == 1
+local using_text_addin = server.teambalance_using_text_addin == 1
 
-local is_intermission = false
-local search_dead_player = false
+local old_botbalance = server.botbalance
 
-local function team_size(team)
-
-	size = 0
-
-	for a,cn in ipairs(server.team_players(team)) do
-		if not (server.player_status_code(cn) == 5) then
-			size = size + 1
-		end
-
-		return size
-	end
-
-	return size
+if old_botbalance == 0
+then
+    server.botbalance = 1
 end
 
-local function other_team(team)
+local team_map = {
+    good	= "evil",
+    evil	= "good"
+}
 
-	if team == "evil" then
-		return "good"
-	else
-		return "evil"
-	end
-end
+local is_intermission
+local search_dead_player
+
 
 local function fuller_team()
 
-	if team_size("good") > team_size("evil") then
-		return "good"
-	else
-		return "evil"
-	end
+    local sizes = server.team_sizes()
+    
+    return _if((sizes.good > sizes.evil), "good", "evil")
 end
 
 local function team_diff()
-	return (math.abs(team_size("good") - team_size("evil")))
+
+    local sizes = server.team_sizes()
+    
+    return (math.abs(sizes.good - sizes.evil))
 end
 
-local function unbalanced()
+local function is_unbalanced()
 
-	if team_diff() > 1 then
-		return true
-	else
-		return false
-	end
-end
-
-local function move_player(cn, team)
-
-	if server.player_team(cn) == team then
-		server.changeteam(cn,other_team(team))
-		server.player_msg(cn,string.format(orange("You switched the team for balance.")))
-
-		search_dead_player = false
-
-		check_balance(5000)
-	end
-end
-
-server.event_handler("frag", function(tcn, acn)
-
-	if search_dead_player == true then
-		move_player(tcn, fuller_team())
-	end
-end)
-
-server.event_handler("suicide", function(cn)
-
-	if search_dead_player == true then
-		move_player(cn, fuller_team())
-	end
-end)
-
-local function check_already_dead_players(team)
-
-	for a, cn in ipairs(server.team_players(team)) do
-		if server.player_status(cn) == "dead" then
-			move_player(cn, team)
-			return
-		end
-	end
-
-	search_dead_player = true
-end
-
-local function balance()
-		   
-	if unbalanced() then
-		if search_dead_player == false then
-			check_already_dead_player(fuller_team())
-		end
-	elseif search_dead_player == true then
-		search_dead_player = false
-	end
-end
-
-local function check_balance(option)
-		   
-	if not option then
---		option = 15000
-		option = 1000
-	end
-
-	server.sleep(option,function()
-		balance()
-	end)
+    return _if((team_diff() > 1), true, nil)
 end
 
 local function is_enabled()
 
-	if gamemodeinfo.teams and not (server.gamemode == "coop edit") and server.mastermode == 0 and is_intermission == false then -- activate conditions
-		return true
-	else
-		search_dead_player = false
-		return false
-	end
+    return _if((gamemodeinfo.teams and (server.mastermode == 0) and not is_intermission), true, nil)
 end
+
+local function move_player(cn, team)
+
+    if server.player_team(cn) == team
+    then
+	server.changeteam(cn, team_map[team])
+	server.player_msg(cn,string.format(orange("You switched the team for balance.")))
+	
+	search_dead_player = nil
+	
+	check_balance(5000)
+    end
+end
+
+local function check_already_dead_players(team)
+
+    for p in server.gteamplayers(team)
+    do
+	if p:status() == "dead"
+	then
+	    move_player(p.cn, team)
+	    return
+	end
+    end
+    
+    search_dead_player = true
+end
+
+local function check_balance(option)
+
+    if not option
+    then
+	option = 1000
+    end
+    
+    server.sleep(option, function()
+    
+	if is_unbalanced()
+	then
+	    if not search_dead_player
+	    then
+		check_already_dead_player(fuller_team())
+	    end
+	    
+	elseif search_dead_player
+	then
+	    search_dead_player = nil
+	end
+    end)
+end
+
+
+server.event_handler("frag", function(tcn, acn)
+
+    if search_dead_player
+    then
+	move_player(tcn, fuller_team())
+    end
+end)
+
+server.event_handler("suicide", function(cn)
+
+    if search_dead_player
+    then
+	move_player(cn, fuller_team())
+    end
+end)
 
 server.event_handler("spectator", function(cn, joined)
 
-	if is_enabled() then
-		if joined == 1 then
-			check_balance()
-		elseif unbalanced() then
-			if using_player_moving_balancer_when_leaving_spec == 1 then
-				move_player(cn, fuller_team())
-			else
-				check_balance(5000)
-			end
-		end
+    if is_enabled()
+    then
+	if joined == 1
+	then
+	    check_balance()
+	    
+	elseif is_unbalanced()
+	then
+	    move_player(cn, fuller_team())
 	end
-
+    end
 end)
 
 server.event_handler("chteamrequest", function(cn, old, new)
 
-	if is_enabled() then
-		if not (new == "good" or new == "evil") then
-			server.player_msg(cn,red("Only teams good and evil are allowed."))
-			return (-1)
-		elseif not (server.player_status_code(cn) == 5) and using_moveblock == 1 then
-			if (math.abs((team_size(old) - 1) - (team_size(new) + 1))) > 1 then
-				server.player_msg(cn, red(string.format("Team change disallowed: \"%s\" team has enough players.", new)))
-				return (-1)
-			end
-		end
+    if is_enabled()
+    then
+	if not team_map[new]
+	then
+	    server.player_msg(cn,red("Only teams good and evil are allowed."))
+	    return (-1)
+	    
+	elseif not (server.player_status_code(cn) == 5) and using_moveblock
+	then
+	    local sizes = server.team_sizes()
+	    
+	    if (math.abs((sizes[old] - 1) - (sizes[new] + 1))) > 1
+	    then
+		server.player_msg(cn, red(string.format("Team change disallowed: \"%s\" team has enough players.", new)))
+		return (-1)
+	    end
 	end
-
+    end
 end)
 
-event.reteam = server.event_handler("reteam", function(cn, old, new)
+server.event_handler("reteam", function(cn, old, new)
 
-	if is_enabled() then
-		check_balance()
-	end
+    if is_enabled()
+    then
+	check_balance()
+    end
 end)
 
-if using_text_addin == 1 then
-	server.event_handler("text", function(cn, text)
-
-		if is_enabled() and unbalanced() then
-			local fuller = fuller_team()
-
-			if server.player_team(cn) == fuller and server.player_status(cn) == "dead" and (string.match(text,"balance") or string.match(text,"BALANCE")) then
-				move_player(cn, fuller)
-
-				check_balance()
-			end
-		end
-
-	end)
+if using_text_addin
+then
+    server.event_handler("text", function(cn, text)
+    
+	if is_enabled() and is_unbalanced()
+	then
+	    local fuller = fuller_team()
+	    
+	    if server.player_team(cn) == fuller and server.player_status(cn) == "dead" and (string.match(text, "balance") or string.match(text, "BALANCE") or string.match(text, "teams") or string.match(text, "TEAMS"))
+	    then
+		move_player(cn, fuller)
+	    end
+	end
+    end)
 end
 
 server.event_handler("finishedgame", function()
 
-	is_intermission = false
-	search_dead_player = false
-
-	if is_enabled() then
-		local flag = 0
-
-		for p in server.aplayers() do
-			local pteam = p:team()
-
-			if not ( (pteam == "good") or (pteam == "evil") ) then
-				if flag == 0 then
-					p:changeteam("good")
-
-					flag = 1
-				else
-					p:changeteam("evil")
-
-					flag = 0
-				end
-			end
-		end
-	end
-
+    is_intermission = nil
+    search_dead_player = nil
 end)
 
 server.event_handler("intermission", function()
-	is_intermission = true
+
+    is_intermission = true
+    search_dead_player = nil
 end)
 
 server.event_handler("mapchange", function(map, mode)
 
-	if is_enabled() then
-		check_balance()
-	end
+    if is_enabled()
+    then
+	check_balance(10000)
+    end
 end)
 
 server.event_handler("disconnect", function(cn, reason)
 
-	if is_enabled() then
-		check_balance()
-	end
+    if is_enabled()
+    then
+	check_balance()
+    end
 end)
 
-local function unload()
-
-	is_intermission = false
-	search_dead_player = false
-end
 
 check_balance(10000) -- in case this module was loaded from #reload
 
-return {unload = unload}
+
+return {unload = function()
+
+    is_intermission = nil
+    search_dead_player = nil
+    
+    server.botbalance = old_botbalance
+end}
