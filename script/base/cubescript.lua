e3503b977b590d09bc95dd54febb737e67e3489d
graham.daws@gmail.com <graham.daws@gmail.com>
2011-01-26 21:32:06 +0000

    Add cubescript initialization
    git-svn-id: http://hopmod.googlecode.com/svn/branches/v4@2119 d7818858-e1b4-11dd-a738-5fe2e3cfbcc5

---
+++ script/base/cubescript.lua
@@ -0,0 +1,41 @@
+
+local library = require("cubescript_library")
+
+-- Give the cubescript environment direct access to the core table
+setmetatable(library, {
+    __index = function(_, value)
+        return core[value] or core.vars[value] or _G[value]
+    end
+})
+
+exec = library.exec
+exec_if_found = library.exec_if_found
+search_paths = library.exec_search_paths
+exec_handler = library.exec_type
+
+function add_exec_search_path(path)
+    search_paths[#search_paths + 1] = path
+end
+
+exec_handler["vars"] = library.exec_cubescript
+
+--[[ Backwards compatibility stuff (deprecated) ]]
+
+execIfFound = exec_if_found
+
+library.global = function(name, value)
+    local property_value = value
+    core.vars[name] = function(value)
+        if value then
+            property_value = value
+            return value
+        else
+            return property_value
+        end
+    end
+end
+
+function cubescript.eval_string(code)
+    return cubescript.eval(code .. "\n", library)
+end
+
