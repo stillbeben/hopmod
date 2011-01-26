bead0cd99523d965eceb6b58278cd3390779b676
graham.daws@gmail.com <graham.daws@gmail.com>
2011-01-26 21:31:29 +0000

    Use the same error function used by cubescript's lua_command_stack::call method
    git-svn-id: http://hopmod.googlecode.com/svn/branches/v4@2115 d7818858-e1b4-11dd-a738-5fe2e3cfbcc5

--- src/hopmod/lua.cpp
+++ src/hopmod/lua.cpp
@@ -75,24 +75,41 @@ lua::event_environment & event_listeners()
 
 int on_error(lua_State * L)
 {
-    lua_getglobal(L, "debug");
-    if(lua_type(L, -1) != LUA_TTABLE)
+    // This source code was copied from cubescript/lua_command_stack.cpp
+    
+    // Enclosing the error message in a table stops subsequent runtime error
+    // functions from altering the error message argument. This is useful for
+    // preserving an error message from the source nested pcall.
+
+    if(lua_type(L, 1) != LUA_TTABLE)
     {
-        lua_pop(L, 1);
+        lua_getglobal(L, "debug");
+        if(lua_type(L, -1) != LUA_TTABLE)
+        {
+            lua_pop(L, 1);
+            return 1;
+        }
+        
+        lua_getfield(L, -1, "traceback");
+        if(lua_type(L, -1) != LUA_TFUNCTION)
+        {
+            lua_pop(L, 1);
+            return 1;
+        }
+        
+        lua_pushvalue(L, 1);
+        lua_pushinteger(L, 2);
+        lua_pcall(L, 2, 1, 0);
+        
+        lua_newtable(L);
+        lua_pushinteger(L, 1);
+        lua_pushvalue(L, -3);
+        lua_settable(L, -3);
+        
         return 1;
     }
     
-    lua_getfield(L, -1, "traceback");
-    if(lua_type(L, -1) != LUA_TFUNCTION)
-    {
-        lua_pop(L, 1);
-        return 1;
-    }
-    
-    lua_pushvalue(L, -3);
-    lua_pushinteger(L, 2);
-    lua_pcall(L, 2, 1, 0);
-    
+    lua_pushvalue(L, 1);
     return 1;
 }
 
