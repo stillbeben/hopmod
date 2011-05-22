afd1e19f906235486466d53d2320cdfd25778e16
graham.daws@gmail.com <graham.daws@gmail.com>
2011-05-22 23:44:32 +0000

    Remove base64 encode and decode functions.
    git-svn-id: http://hopmod.googlecode.com/svn/branches/v4@2318 d7818858-e1b4-11dd-a738-5fe2e3cfbcc5

--- script/base/utils/string.lua
+++ script/base/utils/string.lua
@@ -16,18 +16,6 @@ end
 
 server.format_duration = format_duration
 
-function formatcol(col, text)
-    if text then return "\fs\f" .. col .. text .. "\fr" else return "\f" ..col end
-end
-
-function red(text) return formatcol(3,text) end
-function orange(text) return formatcol(6, text) end
-function green(text) return formatcol(0, text) end
-function white(text) return formatcol(9, text) end
-function yellow(text) return formatcol(2, text) end
-function magenta(text) return formatcol(5, text) end
-function blue(text) return formatcol(1, text) end
-
 function string.split(s, pattern)
     local a = {}
     for x in string.gmatch(s, pattern) do
@@ -114,42 +102,6 @@ do
           return table.concat(Result)
        end
     end
-end
-
-
--- Lua 5.1+ base64 v3.0 (c) 2009 by Alex Kloss <alexthkloss@web.de>
--- licensed under the terms of the LGPL2
--- character table string
-local b='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
-
--- encoding
-function base64_encode(data)
-    return ((data:gsub('.', function(x) 
-        local r,b='',x:byte()
-        for i=8,1,-1 do r=r..(b%2^i-b%2^(i-1)>0 and '1' or '0') end
-        return r;
-    end)..'0000'):gsub('%d%d%d?%d?%d?%d?', function(x)
-        if (#x < 6) then return '' end
-        local c=0
-        for i=1,6 do c=c+(x:sub(i,i)=='1' and 2^(6-i) or 0) end
-        return b:sub(c+1,c+1)
-    end)..({ '', '==', '=' })[#data%3+1])
-end
-
--- decoding
-function base64_decode(data)
-    data = string.gsub(data, '[^'..b..'=]', '')
-    return (data:gsub('.', function(x)
-        if (x == '=') then return '' end
-        local r,f='',(b:find(x)-1)
-        for i=6,1,-1 do r=r..(f%2^i-f%2^(i-1)>0 and '1' or '0') end
-        return r;
-    end):gsub('%d%d%d?%d?%d?%d?%d?%d?', function(x)
-        if (#x ~= 8) then return '' end
-        local c=0
-        for i=1,8 do c=c+(x:sub(i,i)=='1' and 2^(8-i) or 0) end
-        return string.char(c)
-    end))
 end
 
 function strSplit(str, delim, maxNb)
