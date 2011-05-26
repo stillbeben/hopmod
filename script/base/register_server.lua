4a4dde4883c57684e018ba9415ec7f68750abeb9
t.poechtrager@gmail.com <t.poechtrager@gmail.com>
2011-05-26 13:08:11 +0000

    added master-gban support
    git-svn-id: http://hopmod.googlecode.com/svn/branches/v4@2338 d7818858-e1b4-11dd-a738-5fe2e3cfbcc5

--- script/base/register_server.lua
+++ script/base/register_server.lua
@@ -2,11 +2,6 @@ require "net"
 
 local UPDATE = 60*60*1000
 
-local function close_connection(client, callback, error_message)
-    client:close()
-    callback(error_message)
-end
-
 local function register_server(hostname, port, gameport, callback)
 
     local client = net.tcp_client()
@@ -29,23 +24,33 @@ local function register_server(hostname, port, gameport, callback)
                 return
             end
             
-            client:async_read_until("\n", function(line, error_message)
-                
-                if not line then
-                    close_connection(client, callback, error_message or "failed to read reply from server")
-                    return
-                end
-                
-                local command, reason = line:match("([^ ]+)([ \n]*.*)\n")
-                
-                if command == "succreg" then
-                    close_connection(client, callback)
-                elseif command == "failreg" then
-                    close_connection(client, callback, reason or "master server rejected registration")
-                else
-                    close_connection(client, callback, "master server sent unknown reply")
-                end
-            end)
+            local function masterinput()
+                client:async_read_until("\n", function(line, error_message)
+                    
+                    if not line then
+                        client:close()
+                        callback(error_message or "failed to read reply from server")
+                        return
+                    end
+                    
+                    local command, arg1 = line:match("([^ ]+)([ \n]*.*)\n")
+                    
+                    if command == "succreg" then
+                        callback()
+                    elseif command == "failreg" then
+                        callback(arg1 or "master server rejected registration")
+                    elseif command == "addgban" then
+                        local ip = string.sub(arg1, 2, string.len(arg1))
+                        server.ban(ip, -1, "gban")
+                    elseif command == "cleargbans" then
+                    else
+                        callback("master server sent unknown reply")
+                    end
+                    masterinput()
+                end)
+            end
+            masterinput()
+            server.sleep(1000, function() client:close() end)
         end)
     end)
 end
@@ -65,4 +70,3 @@ end
 
 server.interval(UPDATE, update)
 update()
-
