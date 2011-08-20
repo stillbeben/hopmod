d59e5fbc5c7844140c5a06321cec4b9c294c6386
t.poechtrager@gmail.com <t.poechtrager@gmail.com>
2011-08-20 12:53:53 +0000

    reapplied #2407, fixed crashbug (caused by #2409), do not show connect-banner for bots
    git-svn-id: http://hopmod.googlecode.com/svn/branches/v4@2410 d7818858-e1b4-11dd-a738-5fe2e3cfbcc5

--- script/base/banner.lua
+++ script/base/banner.lua
@@ -22,10 +22,10 @@ end
 
 local function onConnect(cn, is_spy)
 
-    if is_spy then return end
+    if is_spy or server.is_bot(cn) then return end
     
     local country = geoip.ip_to_country(server.player_ip(cn))
-    
+ 
     if show_country_message and #country > 0 then
         
         local normal_message = string.format("%s connected from %s.", green(server.player_displayname(cn)), green(country))
