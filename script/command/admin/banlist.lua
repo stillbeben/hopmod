8f2c1bdedac8b3ba68182febcd1976b644cdf009
t.poechtrager@gmail.com <t.poechtrager@gmail.com>
2011-10-09 16:51:43 +0000

    fixed #banlist command (#2)
    git-svn-id: http://hopmod.googlecode.com/svn/branches/v4@2418 d7818858-e1b4-11dd-a738-5fe2e3cfbcc5

--- script/command/admin/banlist.lua
+++ script/command/admin/banlist.lua
@@ -9,7 +9,7 @@ return function(cn)
                                         vars.ban_name = ""
                                 end
                                 local ban_min = round((vars.ban_expire - os.time()) / 60)
-                                server.player_msg(cn, blue() .. "Player: " .. red() .. vars.ban_name .. blue() .. " IP: " .. red() .. ipmask .. blue() " Reason: " .. red() .. vars.ban_reason .. blue() " Time: " .. red() .. vars.ban_time .. blue() .. " Admin: " .. red() .. vars.ban_admin .. blue() .. " Expires in: " .. red() .. ban_min .. " Minutes")
+                                server.player_msg(cn, blue() .. "Player: " .. red() .. vars.ban_name .. blue() .. " IP: " .. red() .. ipmask .. blue() " Reason: " .. red() .. vars.ban_reason .. blue() .. " Time: " .. red() .. vars.ban_time .. blue() .. " Admin: " .. red() .. vars.ban_admin .. blue() .. " Expires in: " .. red() .. ban_min .. " Minutes")
                         end
                 end
     end 
