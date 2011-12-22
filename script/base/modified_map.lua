152d8925051a7fe7aaf4e5c6f1e4eb7e6e068744
t.poechtrager@gmail.com <t.poechtrager@gmail.com>
2011-12-22 14:39:50 +0000

    check against mapcrc provided by ents loader
    git-svn-id: http://hopmod.googlecode.com/svn/branches/v4@2493 d7818858-e1b4-11dd-a738-5fe2e3cfbcc5

--- script/base/modified_map.lua
+++ script/base/modified_map.lua
@@ -15,14 +15,8 @@ server.event_handler("mapcrc", function(cn, map, crc)
     if map ~= server.map or server.gamemode == "coop edit" then
         return
     end
-    
-    local map_info = supported_maps[map]
-        
-    if not map_info or not map_info.crc then
-        return
-    end
 
-    if crypto.tigersum(tostring(crc)) ~= map_info.crc then
+    if crc ~= server.mapcrc then
 
         server.msg(string.format("%s is using a modified map", server.player_displayname(cn)))
         server.log(string.format("%s(%i) is using a modified map (crc %s)", server.player_name(cn), cn, crc))
