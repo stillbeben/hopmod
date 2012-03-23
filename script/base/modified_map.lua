0f7c0816d7516062323574bd971c6b122c6858c2
t.poechtrager@gmail.com <t.poechtrager@gmail.com>
2012-03-23 22:13:55 +0000

    fixed /checkmaps another time
    git-svn-id: http://hopmod.googlecode.com/svn/branches/v4@2574 d7818858-e1b4-11dd-a738-5fe2e3cfbcc5

--- script/base/modified_map.lua
+++ script/base/modified_map.lua
@@ -18,3 +18,19 @@ server.event_handler("modmap", function(cn, map, crc)
     failed_action(cn)
 
 end)
+
+server.event_handler("disconnect", function(cn)
+    modified_clients[server.player_sessionid(cn)] = nil
+end)
+
+server.event_handler("mapchange", function()
+    modified_clients = {}
+end)
+
+server.event_handler("checkmaps", function(cn)
+    for sessionid, cn in pairs(modified_clients) do
+        server.player_msg(cn, string.format("%s is using a modified map", server.player_displayname(cn)))
+    end
+end)
+
+
