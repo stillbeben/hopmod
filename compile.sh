0e086ea874f8d2b9025d259d82413f1e42694f85
pureascii@gmail.com <pureascii@gmail.com>
2013-02-13 22:43:41 +0000

    Fixed compile.sh and allow setting THREADS from outside
    git-svn-id: http://hopmod.googlecode.com/svn/trunk@2677 d7818858-e1b4-11dd-a738-5fe2e3cfbcc5

--- compile.sh
+++ compile.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 export REVISION=`svn info | grep 'Revision: ' | awk '{print $2}'`
-THREADS=`cat /proc/cpuinfo | grep processor | wc -l`
+: ${THREADS:=`cat /proc/cpuinfo | grep processor | wc -l`}
 ARG_LENGTH=$# 
 STRCOMPILE="Compiling"
 COMPILEDIR="release_build"
 COMPILEFLAGS=""
 BUILDTYPE="release"
-if [ "$ARG_LENGTH" > 1 -a "$2" = "debug" ]; then
+if [ "$ARG_LENGTH" -gt 1 -a "$2" = "debug" ]; then
   COMPILEDIR="debug-build"
   COMPILEFLAGS="-D CMAKE_BUILD_TYPE=DEBUG"
   BUILDTYPE="debug"
 fi
-if [ "$ARG_LENGTH" > 0 -a "$1" = 1 ]; then
+if [ "$ARG_LENGTH" -gt 0 -a "$1" = 1 ]; then
   STRCOMPILE="Recompiling"
   rm -rf $COMPILEDIR
 fi
-mkdir $COMPILEDIR
+[ -d $COMPILEDIR ] || mkdir $COMPILEDIR
 cd $COMPILEDIR
 cmake $COMPILEFLAGS .. >> /dev/null
 STRTHREADS="threads"
