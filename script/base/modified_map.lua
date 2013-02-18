3983df28528698bd63432dfa280ba7d6f31358d9
pureascii@gmail.com <pureascii@gmail.com>
2013-02-18 23:51:00 +0000

    Force spectator only during modified map, not for rest of the session
    git-svn-id: http://hopmod.googlecode.com/svn/trunk@2691 d7818858-e1b4-11dd-a738-5fe2e3cfbcc5

--- script/base/modified_map.lua
+++ script/base/modified_map.lua
@@ -1,7 +1,8 @@
 
 local function failed_action(cn)
-    server.force_spec(cn)
-    --modified_clients[server.player_sessionid(cn)] = cn
+    server.player_vars(cn).modmap = server.map
+    -- force_spec is no good idea - no way to decide when a player might be allowed to unspec again
+    server.spec(cn)
 end
 
 server.event_handler("modmap", function(cn, map, crc)
@@ -12,5 +13,13 @@ server.event_handler("modmap", function(cn, map, crc)
 
     server.log(string.format("%s(%i) is using a modified map: %s (crc %s)", server.player_name(cn), cn, map, crc))  
     failed_action(cn)
+
+end)
+-- block mapmod player, when he tries to leave spectator
+server.event_handler("spectator",function(cn,joined)
+
+    if ( joined == 0 ) and ( server.player_vars(cn).modmap == server.map ) then
+        failed_action(cn)
+    end
 
 end)
