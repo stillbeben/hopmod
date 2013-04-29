7426e53872f6a064e7b45db3fcae5283e49c1e3f
blaffablaffa@gmail.com <blaffablaffa@gmail.com>
2013-04-29 19:23:58 +0000

    Control disconnect message spamming with flood_protect_disc_* variables
    git-svn-id: http://hopmod.googlecode.com/svn/trunk@2734 d7818858-e1b4-11dd-a738-5fe2e3cfbcc5

--- src/hopmod/server_functions.hpp
+++ src/hopmod/server_functions.hpp
@@ -57,6 +57,9 @@ namespace server
     extern int spectator_delay;
     
     namespace message{
+
+        extern int disc_msgs;
+        extern int disc_window;
         
         namespace resend_time{
             
