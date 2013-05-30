c9651f6488415589f31652e0c9e07eec10d373da
blaffablaffa@gmail.com <blaffablaffa@gmail.com>
2013-05-30 22:30:24 +0000

    fix luasql execution (installation still has to be manual)
    git-svn-id: http://hopmod.googlecode.com/svn/trunk@2774 d7818858-e1b4-11dd-a738-5fe2e3cfbcc5

--- script/base/utils/mysql.lua
+++ script/base/utils/mysql.lua
@@ -1,4 +1,5 @@
 mysql = {}
+local luasql
 
 -- Copied from http://lua-users.org/wiki/GenericInputAlgorithms
 function mysql.rows(cursor)
@@ -69,8 +70,8 @@ local function install_db(connection)
 end
 
 function mysql.open(connection, install)
-    
-    require "luasql_mysql"
+
+    luasql = require "luasql_mysql"
     
     connection.handler = luasql.mysql():connect(connection.settings.database, connection.settings.username, connection.settings.password, connection.settings.hostname, connection.settings.port)
     
